db27abf13f159ae8389cdcb7d1afd92187dea0de
arensb <arensb@9309df22-992a-11dd-b31e-5b0f3c431db7>
1999-01-24 04:32:47 +0000

    Created encapsulation type files.

---
+++ trunk/SLP_private.h
@@ -0,0 +1,12 @@
+/* SLP_private.h
+ *
+ * $Id: SLP_private.h,v 1.1 1999-01-24 04:32:47 arensb Exp $
+ */
+#ifndef _SLP_private_h_
+#define _SLP_private_h_
+
+typedef struct SLPPart
+{
+} SLPPart;
+
+#endif	/* _SLP_private_h_ */
