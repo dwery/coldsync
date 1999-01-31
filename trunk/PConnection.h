c07ffaa3164dfe856911a3af3eb2bdc8dad3873d
arensb <arensb@9309df22-992a-11dd-b31e-5b0f3c431db7>
1999-01-31 21:54:35 +0000

    Simplified PConnection somewhat. Use ints as handle descriptors.

--- trunk/PConnection.h
+++ trunk/PConnection.h
@@ -3,48 +3,71 @@
  * Defines the PConnection abstraction, which embodies a connection
  * to a P device.
  *
- * $Id: PConnection.h,v 1.1 1999-01-24 04:56:48 arensb Exp $
+ * $Id: PConnection.h,v 1.2 1999-01-31 21:54:35 arensb Exp $
  */
 #ifndef _PConn_h_
 #define _PConn_h_
 
-#include "SLP_private.h"
-#include "PADP_private.h"
-#include "CMP_private.h"
-#include "DLP_private.h"
+#include <termios.h>
+/*  #include <sys/protosw.h> */
+#include "palm_types.h"
+#include "slp.h"
 
 /* PConnection
  * This struct is an opaque type that contains all of the state about
- * a connection to a given Palm device at a given time. There is a
- * common part and several per-protocol parts, to encourage
- * encapsulation.
+ * a connection to a given Palm device at a given time.
  * Programs should never use the PConnection type directly.
  * Instead, everything should use the PConnHandle type, defined
  * below.
  */
+/* XXX - It'd be way cool to turn this into an honest-to-God socket */
 struct PConnection
 {
 	/* Common part */
-	/* XXX - Nothing yet */
-/*  	struct PConnection *next; */
+	struct PConnection *next;	/* Next PConnection on linked
+					 * list */
+	int fd;				/* File descriptor */
 
 	/* Protocol-dependent parts */
-	SLPPart slp;
-	PADPPart padp;
-	CMPPart cmp;
-	DLPPart dlp;
+
+	/* Packet Assembly/Disassembly Protocol (PADP) */
+	struct {
+		ubyte xid;	/* Transaction ID. PADP sets this, and
+				 * SLP reads it when sending out a
+				 * packet.
+				 * This violates encapsulation, but I
+				 * can't think of a better way to do
+				 * it.
+				 */
+	} padp;
+
+	/* Serial Link Protocol (SLP) */
+	struct {
+		struct slp_addr local_addr;
+		struct slp_addr remote_addr;
+
+		/* Convenience stuff for reading and writing. */
+		struct slp_header header;	
+		ubyte buf[SLP_MAX_PACKET_LEN];
+		uword crc;
+
+		ubyte last_xid;	/* The transaction ID of the last SLP
+				 * packet that was received. PADP uses
+				 * this for replies.
+				 * This is a gross hack that violates
+				 * encapsulation, but it's necessary
+				 * since SLP and PADP are so closely
+				 * tied.
+				 */
+	} slp;
 };
 
-/* PConnHandle
- * This is an opaque type used to refer to a particular connection.
- * Programs should not try to peek inside (just like X11's Widgets),
- * nor should they assume that this is a pointer.
- * XXX - At some point, it would be nice if this could become an
- * honest to God socket type, at which point you'd just have
- *	typedef int PConnHandle;
- */
-typedef struct PConnection *PConnHandle;
+/*  typedef struct PConnection *PConnHandle; */
 
-extern PConnHandle new_PConnection(/*XXX*/);
+extern struct PConnection *PConnLookup(int fd);
+extern int new_PConnection(char *fname);
+extern int PConnClose(int fd);
+extern int PConn_bind(int fd, struct slp_addr *addr);
+extern int PConnSetSpeed(int fd, speed_t speed);	/* XXX */
 
 #endif	/* _PConn_h_ */
