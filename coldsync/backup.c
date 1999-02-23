7d2005bf1c8550ac7b1327ce908495f348bf260b
arensb <arensb@9309df22-992a-11dd-b31e-5b0f3c431db7>
1999-02-23 00:52:38 +0000

    Took out old Cold_BackupDB(), finished Cold_RecordBackup().

--- coldsync/backup.c
+++ coldsync/backup.c
@@ -3,11 +3,11 @@
  * Functions for backing up Palm databases (both .pdb and .prc) from
  * the Palm to the desktop.
  *
- * $Id: backup.c,v 1.1 1999-02-22 10:27:13 arensb Exp $
+ * $Id: backup.c,v 1.2 1999-02-23 00:52:38 arensb Exp $
  */
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
+#include <fcntl.h>		/* For open() */
 #include <sys/types.h>
 #include <sys/uio.h>
 #include <unistd.h>
@@ -19,281 +19,6 @@
 #include "palm/pdb.h"
 #include "coldsync.h"
 
-#if 0
-int
-Cold_BackupDB(struct PConnection *pconn,
-	      struct ColdPalm *palm,
-	      struct dlp_dbinfo *dbinfo,
-	      const char *fname)
-{
-extern int padp_debug;
-extern int dlp_debug;
-extern int dlpc_debug;
-	int i;
-	int err;
-	int outfd;		/* Backup file descriptor */
-	static ubyte outbuf[2048];	/* XXX - Figure out how big this ought to be */
-	const ubyte *rptr;	/* Pointer into buffers, for reading */
-	ubyte *wptr;		/* Pointer into buffers, for writing */
-	ubyte dbh;		/* Database handle */
-	struct dlp_opendbinfo opendbinfo;
-				/* Info about the currently-open database */
-	uword num;
-	struct dlp_appblock appblock;	/* AppInfo block */
-	uword appblock_len;
-	ubyte *appblock_data;
-	struct dlp_sortblock sortblock;	/* Sort block */
-	uword sortblock_len;
-	ubyte *sortblock_data;
-	udword *idlist;		/* Array of record IDs */
-
-/* XXX - Should abort more cleanly on error: let the Palm know and stuff */
-
-	printf("** Backing up \"%s\" to \"%s\"\n",
-	       dbinfo->name, fname);
-padp_debug = 0;
-dlp_debug = 10;
-dlpc_debug = 10;
-
-	/* XXX - Would it be easier to read the entire database into memory
-	 * first, and then write it to the file? Probably.
-	 */
-
-	/* Tell the Palm we're running a conduit. Yes, HotSync does this
-	 * before opening the database.
-	 */
-	if ((err = DlpOpenConduit(pconn)) < 0)
-	{
-		fprintf(stderr, "Can't OpenConduit(\"%s\"): err %d, palm_errno %d\n",
-			dbinfo->name, err, palm_errno);
-		return -1;
-	}
-
-	/* Open the database */
-	/* XXX - Memory card 0 is hard-wired. Bad! */
-	err = DlpOpenDB(pconn, 0, dbinfo->name, DLPCMD_OPENDBFLAG_READ, &dbh);
-	if (err < 0)
-	{
-		fprintf(stderr, "Can't open Palm database \"%s\": err %d, palm_errno %d\n",
-			dbinfo->name, err, palm_errno);
-		return -1;
-	}
-
-	/* XXX - Ought to be more paranoid: back the database up to a
-	 * temporary file, then move it into place once the backup has
-	 * completed successfully. That way, if this fails during a full
-	 * backup when there are already databases in place, the
-	 * information that's already there won't get clobbered.
-	 */
-	/* Create the file */
-	outfd = open(fname, O_RDWR | O_CREAT /*| O_EXLOCK*/, 0666);
-	if (outfd < 0)
-	{
-		perror("Cold_BackupDB: open");
-		return -1;
-	}
-
-	/* Find out how many records/resources there are in the database */
-	err = DlpReadOpenDBInfo(pconn, dbh, &opendbinfo);
-	if (err < 0)
-	{
-		fprintf(stderr, "Error getting open DB info for \"%s\": %d\n",
-			dbinfo->name, err);
-		/* XXX - Delete the file */
-		return -1;
-	}
-printf("There are %d records\n", opendbinfo.numrecs);
-
-	/* Get the AppInfo block, if it exists */
-	appblock.dbid = dbh;
-	appblock.offset = 0;
-	appblock.len = ~0;	/* Read the whole thing */
-	appblock_len = 0;
-	if ((err = DlpReadAppBlock(pconn, &appblock, &appblock_len, &rptr)) < 0)
-	{
-		fprintf(stderr, "Error reading AppBlock \"%s\": %d\n",
-			dbinfo->name, err);
-/*  		return -1; */
-	}
-
-	/* Save a copy of the app block */
-	appblock_data = NULL;
-	if (appblock_len > 0)
-	{
-		if ((appblock_data = (ubyte *) malloc(appblock_len)) == NULL)
-		{
-			perror("appblock malloc");
-			return -1;
-		}
-		memcpy(appblock_data, rptr, appblock_len);
-	}
-
-	/* Get the sort block, if it exists */
-	sortblock.dbid = dbh;
-	sortblock.offset = 0;
-	sortblock.len = ~0;	/* Read the whole thing */
-	sortblock_len = 0;
-	if ((err = DlpReadSortBlock(pconn, &sortblock, &sortblock_len, &rptr)) < 0)
-	{
-		fprintf(stderr, "Error reading SortBlock \"%s\": %d\n",
-			dbinfo->name, err);
-/*  		return -1; */
-	}
-	/* Save a copy of the sort block */
-	sortblock_data = NULL;
-	if (sortblock_len > 0)
-	{
-		if ((sortblock_data = (ubyte *) malloc(sortblock_len)) == NULL)
-		{
-			perror("sortblock malloc");
-			return -1;
-		}
-		memcpy(sortblock_data, rptr, sortblock_len);
-	}
-
-	/* XXX - Get list of record IDs */
-	if (((dbinfo->db_flags & DLPCMD_DBFLAG_RESDB) == 0) &&
-	    opendbinfo.numrecs > 0)
-	{
-		/* Allocate space for the list of record IDs */
-		if ((idlist = (udword *) malloc(opendbinfo.numrecs * sizeof(udword)))
-		    == NULL)
-		{
-			perror("Can't allocate record ID list");
-			return -1;
-		}
-		err = DlpReadRecordIDList(pconn, dbh, 0x00, 0, ~0, &num, idlist);
-		if (err < 0)
-		{
-			fprintf(stderr, "Error in ReadRecordIDList \"%s\": %d\n",
-				dbinfo->name, err);
-		}
-printf("# Record list:\n");
-for (i = 0; i < opendbinfo.numrecs; i++)
-printf("\t%d: 0x%08lx\n", i, idlist[i]);
-	}
-
-	/* XXX - Write what we've got so far */
-	wptr = outbuf;
-	memcpy(wptr, dbinfo->name, PDB_DBNAMELEN);
-	wptr += PDB_DBNAMELEN;
-	put_uword(&wptr, dbinfo->db_flags);
-	put_uword(&wptr, dbinfo->version);
-	put_udword(&wptr, time_dlp2palmtime(&dbinfo->ctime));
-	put_udword(&wptr, time_dlp2palmtime(&dbinfo->mtime));
-	put_udword(&wptr, time_dlp2palmtime(&dbinfo->baktime));
-	put_udword(&wptr, dbinfo->modnum);
-	put_udword(&wptr, 0L);	/* appinfoID: this will be filled in later */
-	put_udword(&wptr, 0L);	/* sortinfoID: this will be filled in later */
-	put_udword(&wptr, dbinfo->type);
-	put_udword(&wptr, dbinfo->creator);
-	put_udword(&wptr, 0L);	/* uniqueIDseed */
-				/* XXX - Is there a good way to get this?
-				 * Should this just be a random number?
-				 */
-	/* Write the database header */
-	/* XXX - What should write()'s size argument be? */
-	if ((err = write(outfd, outbuf, wptr - outbuf)) < 0)
-	{
-		perror("Cold_BackupDB: write");
-		close(outfd);
-		/* XXX - Delete the file */
-		return -1;
-	}
-
-	/* Construct the record/resource index header */
-	wptr = outbuf;
-	put_udword(&wptr, 0L);		/* nextID: when is it not 0? */
-	put_uword(&wptr, opendbinfo.numrecs);
-
-	/* Write the record/resource index header */
-	/* XXX - Fixed size for write()'s size argument */
-	if ((err = write(outfd, outbuf, wptr - outbuf)) < 0)
-	{
-		perror("Cold_BackupDB: write");
-		close(outfd);
-		/* XXX - Delete the file */
-		return -1;
-	}
-
-	/* Leave space for record/resource index; we'll come back and fill
-	 * it in later.
-	 */
-	if (dbinfo->db_flags & DLPCMD_DBFLAG_RESDB)
-	{
-		/* This is a resource database */
-		/* XXX */
-/*  		lseek(fd, , SEEK_CUR); */
-	} else {
-		/* This is a record database */
-		/* XXX */
-	}
-
-	/* Write the AppInfo block, if any */
-	if (appblock_len > 0)
-	{
-		write(outfd, appblock_data, appblock_len);
-		/* XXX - Error-checking */
-	}
-
-	/* Write the sort block, if any */
-	if (sortblock_len > 0)
-	{
-		write(outfd, sortblock_data, sortblock_len);
-		/* XXX - Error-checking */
-	}
-
-	/* XXX - Read each record in turn */
-	if (((dbinfo->db_flags & DLPCMD_DBFLAG_RESDB) == 0) &&
-	    opendbinfo.numrecs > 0)
-	{
-		for (i = 0; i < opendbinfo.numrecs; i++)
-		{
-			struct dlp_readrecreq_byid byid;	/* XXX - Bogus API */
-			struct dlp_readrecret record;
-
-			/* Read a record */
-			byid.dbid = dbh;
-			byid.recid = idlist[i];
-			byid.offset = 0;
-			/* XXX - This length is bogus, and is just here to
-			 * work around the fact that the PADP layer doesn't
-			 * handle multi-fragment packets yet.
-			 * XXX - This should really loop and read each bit
-			 * of the record in turn (for now).
-			 */
-			byid.len = 512;		/* XXX - Need const for "everything"*/
-			fprintf(stderr, "Reading record ID 0x%08lx\n", byid.recid);
-			if ((err = DlpReadRecordByID(pconn, &byid, &record)) < 0)
-			{
-				fprintf(stderr, "Error in ReadRecordByID \"%s\": %d\n",
-					dbinfo->name, err);
-				/* XXX - Better error handling */
-				return -1;
-			}
-
-			/* Write the record */
-			/* XXX - Error-checking */
-			write(outfd, record.data, record.size);
-		}
-	}
-
-	/* Close the file */
-	close(outfd);
-
-	/* XXX - Reset the sync flags */
-
-	if ((err = DlpCloseDB(pconn, dbh)) < 0)
-	{
-		fprintf(stderr, "Error closing Palm database \"%s\": %d\n",
-			dbinfo->name, err);
-		return -1;
-	}
-
-	return 0;
-}
-#endif	/* 0 */
-
 int
 Cold_RecordBackup(struct PConnection *pconn,
 		  struct ColdPalm *palm,
@@ -304,6 +29,7 @@ Cold_RecordBackup(struct PConnection *pconn,
 	int err;
 	ubyte dbh = ~0;		/* Database handle */
 	const ubyte *rptr;	/* Pointer into buffers, for reading */
+	ubyte *wptr;		/* Pointer into buffers, for writing */
 	uword num;		/* Generic: # things read */
 	udword ctime;		/* Creation time, in seconds */
 	udword mtime;		/* Modification time, in seconds */
@@ -314,6 +40,7 @@ Cold_RecordBackup(struct PConnection *pconn,
 				/* Length of sort block */
 	ubyte *sortblock = NULL;
 				/* Sort block */
+	int outfd = -1;		/* Output file descriptor */
 	struct dlp_opendbinfo opendbinfo;
 				/* Info about open database (well, the # of
 				 * records in it). */
@@ -322,6 +49,14 @@ Cold_RecordBackup(struct PConnection *pconn,
 	struct dlp_recinfo *recinfo = NULL;
 				/* Info struct for each record */
 	ubyte **recdata = NULL;	/* Data for each record */
+	static ubyte header_buf[PDB_HEADER_LEN];
+				/* Buffer for outgoing database header */
+	static ubyte rl_header_buf[PDB_RECORDLIST_LEN];
+				/* Buffer for outgoing record list header */
+	static ubyte recix_buf[PDB_RECORDIX_LEN];
+				/* Buffer for outgoing record info struct */
+	udword next_offset;	/* Offset of the next thing that gets
+				 * written */
 
 printf("--> Backing up record database %s to %s\n",
        dbinfo->name, bakfname);
@@ -375,6 +110,14 @@ printf("--> Backing up record database %s to %s\n",
 	switch (err)
 	{
 	    case DLPSTAT_NOERR:
+		/* Make a copy of the AppInfo block */
+		if ((appblock = (ubyte *) malloc(appblock_len)) == NULL)
+		{
+			fprintf(stderr, "** Out of memory\n");
+			err = -1;
+			goto abort;
+		}
+		memcpy(appblock, rptr, appblock_len);
 		break;
 	    case DLPSTAT_NOTFOUND:
 		/* This database doesn't have an AppInfo block */
@@ -388,14 +131,6 @@ printf("### %s doesn't have an AppInfo block\n", dbinfo->name);
 		err = -1;
 		goto abort;
 	}
-	/* Make a copy of the AppInfo block */
-	if ((appblock = (ubyte *) malloc(appblock_len)) == NULL)
-	{
-		fprintf(stderr, "** Out of memory\n");
-		err = -1;
-		goto abort;
-	}
-	memcpy(appblock, rptr, appblock_len);
 
 	/* Try to get the sort block */
 	/* XXX - Need a const for "everything" */
@@ -403,6 +138,14 @@ printf("### %s doesn't have an AppInfo block\n", dbinfo->name);
 	switch (err)
 	{
 	    case DLPSTAT_NOERR:
+		/* Make a copy of the sort block */
+		if ((sortblock = (ubyte *) malloc(sortblock_len)) == NULL)
+		{
+			fprintf(stderr, "** Out of memory\n");
+			err = -1;
+			goto abort;
+		}
+		memcpy(sortblock, rptr, sortblock_len);
 		break;
 	    case DLPSTAT_NOTFOUND:
 		/* This database doesn't have an SortInfo block */
@@ -416,14 +159,6 @@ printf("### %s doesn't have a sort block\n", dbinfo->name);
 		err = -1;
 		goto abort;
 	}
-	/* Make a copy of the sort block */
-	if ((sortblock = (ubyte *) malloc(sortblock_len)) == NULL)
-	{
-		fprintf(stderr, "** Out of memory\n");
-		err = -1;
-		goto abort;
-	}
-	memcpy(sortblock, rptr, sortblock_len);
 
 	/* XXX - Index header info:
 	   nextID (probably safe to set to 0)
@@ -528,14 +263,169 @@ padp_debug = 100;
 	   - attributes: DlpReadRecordByID
 	   - unique ID: DlpReadRecordByID
 	 */
-	/* XXX - AppInfo block, if any */
-	/* XXX - Sort block, if any */
-	/* XXX - List of records */
+
+	/* Open the backup file */
+	if ((outfd = open(bakfname, O_WRONLY | O_CREAT, 0666)) < 0)
+	{
+		fprintf(stderr, "Can't open \"%s\" for writing\n",
+			bakfname);
+		perror("open");
+		err = -1;
+		goto abort;
+	}
+
+	/* Initialize next_offset: the first thing we'll need an offset for
+	 * (probably the AppInfo block) will go after the two useless NULs.
+	 */
+	next_offset = PDB_HEADER_LEN + PDB_RECORDLIST_LEN +
+		(num_recs * PDB_RECORDIX_LEN) + 2;
+
+	/* Construct the database header */
+	wptr = header_buf;
+	memcpy(wptr, dbinfo->name, PDB_DBNAMELEN);	/* name */
+	wptr += PDB_DBNAMELEN;
+	put_uword(&wptr, dbinfo->db_flags);		/* attributes */
+	put_uword(&wptr, dbinfo->version);		/* version */
+	put_udword(&wptr, ctime);			/* ctime */
+	put_udword(&wptr, mtime);			/* mtime */
+	put_udword(&wptr, baktime);			/* baktime */
+	put_udword(&wptr, dbinfo->modnum);		/* modnum */
+	if (appblock == NULL)
+		put_udword(&wptr, 0L);			/* appinfoID */
+	else {
+		put_udword(&wptr, next_offset);		/* appinfoID */
+		next_offset += appblock_len;	/* Update next_offset */
+	}
+	if (sortblock == NULL)
+{
+fprintf(stderr, "no sortblock offset\n");
+		put_udword(&wptr, 0L);			/* sortinfoID */
+}
+	else {
+fprintf(stderr, "sortblock's offset: 0x%08lx\n", next_offset);
+		put_udword(&wptr, next_offset);		/* sortinfoID */
+		next_offset += sortblock_len;	/* Update next_offset */
+	}
+	put_udword(&wptr, dbinfo->type);		/* type */
+	put_udword(&wptr, dbinfo->creator);		/* creator */
+	put_udword(&wptr, 0L);				/* uniqueIDseed */
+
+	/* Write the database header */
+	if ((err = write(outfd, header_buf, PDB_HEADER_LEN)) != PDB_HEADER_LEN)
+	{
+		perror("write");
+		goto abort;
+	}
+
+	/* Construct the record index header */
+	wptr = rl_header_buf;
+	put_udword(&wptr, 0L);		/* nextID */
+					/* XXX - Should this be something else? */
+	put_uword(&wptr, num_recs);	/* len */
+
+	/* Write the record index header */
+	if ((err = write(outfd, rl_header_buf, PDB_RECORDLIST_LEN)) !=
+	    PDB_RECORDLIST_LEN)
+	{
+		perror("write");
+		goto abort;
+	}
+
+	/* Write the record index, one record at a time */
+	for (i = 0; i < num_recs; i++)
+	{
+		wptr = recix_buf;
+		put_udword(&wptr, next_offset);	/* offset */
+		next_offset += recinfo[i].size;	/* Update next_offset */
+		put_ubyte(&wptr, recinfo[i].attributes);
+						/* attributes */
+		/* Lowest 3 bytes of uniqueID */
+		put_ubyte(&wptr, (recinfo[i].id >> 16) & 0xff);
+		put_ubyte(&wptr, (recinfo[i].id >> 8) & 0xff);
+		put_ubyte(&wptr, recinfo[i].id & 0xff);
+
+		/* Write the record index entry */
+		if ((err = write(outfd, recix_buf, PDB_RECORDIX_LEN)) !=
+		    PDB_RECORDIX_LEN)
+		{
+			perror("write");
+			goto abort;
+		}
+	}
+
+	/* Write those useless two NULs */
+	wptr = header_buf;
+	put_uword(&wptr, 0);
+	if ((err = write(outfd, header_buf, 2)) < 0)
+	{
+		perror("write");
+		goto abort;
+	}
+
+	/* Write the AppInfo block, if there is one */
+	if (appblock != NULL)
+		if ((err = write(outfd, appblock, appblock_len)) !=
+		    appblock_len)
+		{
+			perror("write");
+			goto abort;
+		}
+
+	/* Write the sort block, if there is one */
+	if (sortblock != NULL)
+		if ((err = write(outfd, sortblock, sortblock_len)) !=
+		    sortblock_len)
+		{
+			perror("write");
+			goto abort;
+		}
+
+	/* Write each record in turn */
+	for (i = 0; i < num_recs; i++)
+	{
+		if ((err = write(outfd, recdata[i], recinfo[i].size)) !=
+		    recinfo[i].size)
+		{
+			perror("write");
+			goto abort;
+		}
+	}
+
+	/* XXX - I'm not sure about these two, nor their relative order.
+	 * HotSync appears to do both at different times.
+	 */
+/* XXX - It appears to work if you clean the database, only. Anything else
+ * causes it to reset.
+ */
+#if 0
+fprintf(stderr, "### Cleaning up database\n");
+	err = DlpCleanUpDatabase(pconn, dbh);
+	if (err != DLPSTAT_NOERR)
+	{
+		fprintf(stderr, "Can't clean up database.\n");
+		err = -1;
+		goto abort;
+	}
+#endif	/* 0 */
+
+#if 0
+fprintf(stderr, "### Resetting sync flags\n");
+	err = DlpResetSyncFlags(pconn, dbh);
+	if (err != DLPSTAT_NOERR)
+	{
+		fprintf(stderr, "Can't reset sync flags.\n");
+		err = -1;
+		goto abort;
+	}
+#endif	/* 0 */
 
   abort:	/* In case of error, the code above just jumps to here.
 		 * Clean everything up in the reverse order of how it was
 		 * initialized.
 		 */
+	if (outfd >= 0)			/* Close the file */
+		close(outfd);
+
 	if (recdata != NULL)		/* It's an array of pointers */
 	{
 		for (i = 0; i < num_recs; i++)
@@ -554,7 +444,8 @@ padp_debug = 100;
 		free(appblock);
 
 	/* Close the database */
-	DlpCloseDB(pconn, dbh);		/* Don't really care if this fails */
+	if (dbh != 0xffff)
+		DlpCloseDB(pconn, dbh);	/* Don't really care if this fails */
 
 	return err;
 }
