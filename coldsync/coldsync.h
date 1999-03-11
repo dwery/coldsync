a3e51da79ce8ce5f3528bf407e2fe524ab50eca1
arensb <arensb@9309df22-992a-11dd-b31e-5b0f3c431db7>
1999-03-11 03:40:14 +0000

    Renamed ColdPalm to Palm. Added various bits of useful information about a given Palm device. Added a bunch of prototypes.

--- coldsync/coldsync.h
+++ coldsync/coldsync.h
@@ -2,32 +2,29 @@
  *
  * Data structures and such needed by 'coldsync'.
  *
- * $Id: coldsync.h,v 1.3 1999-02-24 13:20:04 arensb Exp $
+ * $Id: coldsync.h,v 1.4 1999-03-11 03:40:14 arensb Exp $
  */
 #ifndef _coldsync_h_
 #define _coldsync_h_
 
 #include "pconn/PConnection.h"
+#include "pconn/dlp_cmd.h"
 
-#define BACKUP_DIR	"palm_backup"	/* XXX - This should be gotten from
-					 * the password info and the config
-					 * file. It should also be an
-					 * absolute pathname.
-					 */
 #define INSTALL_DIR	"palm_install"	/* XXX - This should be gotten from
 					 * the password info and the config
 					 * file. It should also be an
 					 * absolute pathname.
 					 */
 
-/* ColdPalm
+/* Palm
  * Information about the Palm being currently synced.
  */
-struct ColdPalm
+struct Palm
 {
-	/* XXX - System information */
-	/* XXX - User information */
-	/* XXX - NetSync information */
+	struct dlp_sysinfo sysinfo;	/* System information */
+	struct dlp_userinfo userinfo;	/* User information */
+	struct dlp_netsyncinfo netsyncinfo;
+					/* NetSync information */
 
 	/* Memory information */
 	int num_cards;			/* # memory cards */
@@ -43,22 +40,29 @@ struct ColdPalm
 
 extern int Cold_Connect(struct PConnection *pconn, const char *name);
 extern int Cold_Disconnect(struct PConnection *pconn, const ubyte status);
-extern int Cold_GetMemInfo(struct PConnection *pconn, struct ColdPalm *palm);
-extern int Cold_ListDBs(struct PConnection *pconn, struct ColdPalm *palm);
-extern int Cold_HandleDB(struct PConnection *pconn, struct ColdPalm *palm,
+extern int Cold_GetMemInfo(struct PConnection *pconn, struct Palm *palm);
+extern int Cold_ListDBs(struct PConnection *pconn, struct Palm *palm);
+extern int Cold_HandleDB(struct PConnection *pconn, struct Palm *palm,
 			 const int dbnum);
-extern int Cold_BackupDB(struct PConnection *pconn, struct ColdPalm *palm,
+extern int Cold_BackupDB(struct PConnection *pconn, struct Palm *palm,
 			 struct dlp_dbinfo *dbinfo, const char *fname);
-extern int Cold_SyncDB(struct PConnection *pconn, struct ColdPalm *palm,
+extern int Cold_SyncDB(struct PConnection *pconn, struct Palm *palm,
 		       const int dbnum);
-extern int Cold_RecordBackup(struct PConnection *pconn,
-			     struct ColdPalm *palm,
-			     struct dlp_dbinfo *dbinfo,
-			     char *bakfname);
-extern int Cold_ResourceBackup(struct PConnection *pconn,
-			       struct ColdPalm *palm,
-			       struct dlp_dbinfo *dbinfo,
-			       char *bakfname);
+extern int Cold_Backup(struct PConnection *pconn,
+		       struct Palm *palm,
+		       struct dlp_dbinfo *dbinfo,
+		       char *bakfname);
+extern int Cold_RecordSync(struct PConnection *pconn,
+			   struct Palm *palm,
+			   struct dlp_dbinfo *dbinfo,
+			   char *bakfname);
+struct pdb *LoadDatabase(char *fname);
+extern int SlowSync(struct PConnection *pconn,
+		    struct dlp_dbinfo *remotedb,
+		    struct pdb *localdb,
+		    char *bakfname);
+extern struct pdb *DownloadRecordDB(struct PConnection *pconn,
+				    struct dlp_dbinfo *dbinfo);
 
 #endif	/* _coldsync_h_ */
 
