9fe6b8a6db1ff60e39fcda6470634f137edb57a5
arensb <arensb@9309df22-992a-11dd-b31e-5b0f3c431db7>
1999-05-31 20:57:39 +0000

    The various per-user sync directories are now variables, rather than #defines. Now automatically checks to see whether a slow or fast sync is required. Updates user info (e.g., user name).

--- coldsync/coldsync.h
+++ coldsync/coldsync.h
@@ -2,10 +2,14 @@
  *
  * Data structures and such needed by 'coldsync'.
  *
- * $Id: coldsync.h,v 1.6 1999-03-16 11:03:40 arensb Exp $
+ * $Id: coldsync.h,v 1.7 1999-05-31 20:57:39 arensb Exp $
  */
 #ifndef _coldsync_h_
 #define _coldsync_h_
+
+#include <unistd.h>		/* For uid_t */
+#include <sys/types.h>		/* For uid_t */
+#include <sys/param.h>		/* For MAXPATHLEN */
 
 #include "pconn/PConnection.h"
 #include "pconn/dlp_cmd.h"
@@ -38,6 +42,21 @@ struct Palm
 	struct dlp_dbinfo *dblist;	/* Database list */
 };
 
+extern int need_slow_sync;
+extern udword hostid;		/* This host's ID */
+				/* XXX - This shouldn't be global */
+extern uid_t user_uid;		/* Owner's UID */
+extern char user_fullname[DLPCMD_USERNAME_LEN];
+				/* Owner's full name */
+
+/* Configuration variables */
+/* XXX - There's probably a better place to put them */
+extern char palmdir[MAXPATHLEN+1];	/* ~/.palm pathname */
+extern char backupdir[MAXPATHLEN+1];	/* ~/.palm/backup pathname */
+extern char archivedir[MAXPATHLEN+1];	/* ~/.palm/archive pathname */
+extern char installdir[MAXPATHLEN+1];	/* ~/.palm/install pathname */
+
+/* Function prototypes */
 extern int Connect(struct PConnection *pconn, const char *name);
 extern int Disconnect(struct PConnection *pconn, const ubyte status);
 extern int GetMemInfo(struct PConnection *pconn, struct Palm *palm);
