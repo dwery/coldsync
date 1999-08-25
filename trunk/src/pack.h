74ca6b279826e99397980795c0d3a2ef652b1003
arensb <arensb@9309df22-992a-11dd-b31e-5b0f3c431db7>
1999-08-25 04:06:07 +0000

    Added PACK_TIME, for reading and writing 'struct dlp_time's.

--- trunk/src/pack.h
+++ trunk/src/pack.h
@@ -1,6 +1,8 @@
 /* pack.h
  *
- * $Id: pack.h,v 2.1 1999-08-23 08:44:51 arensb Exp $
+ * Declarations for ppack() and punpack().
+ *
+ * $Id: pack.h,v 2.2 1999-08-25 04:06:07 arensb Exp $
  */
 #ifndef _pack_h_
 #define _pack_h_
@@ -12,7 +14,8 @@
 #define PACK_UWORDS(n)	(PACK_UWORD | 0x80), (n)
 #define PACK_UDWORD	3
 #define PACK_UDWORDS(n)	(PACK_UDWORD | 0x80), (n)
-
+#define PACK_TIME	4
+#define PACK_TIMES(n)	(PACK_TIME | 0x80), (n)
 
 extern int ppack(ubyte *buf, ...);
 extern int punpack(const ubyte *buf, ...);
