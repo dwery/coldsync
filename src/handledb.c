a59ab1cc4575ee8310696c5bd4af8b1492f16f09
arensb <arensb@9309df22-992a-11dd-b31e-5b0f3c431db7>
2000-01-27 02:40:16 +0000

    Added mkfname(), mkarchfname(). Fixed mkbakfname() to use mkfname(). Now escapes weird characters in file names.

--- src/handledb.c
+++ src/handledb.c
@@ -6,13 +6,14 @@
  *	You may distribute this file under the terms of the Artistic
  *	License, as specified in the README file.
  *
- * $Id: handledb.c,v 1.12 2000-01-22 05:16:00 arensb Exp $
+ * $Id: handledb.c,v 1.13 2000-01-27 02:40:16 arensb Exp $
  */
 
 #include "config.h"
 #include <stdio.h>
 #include <string.h>		/* For strncpy() and friends */
 #include <sys/param.h>		/* For MAXPATHLEN */
+#include <ctype.h>		/* For isprint() and friends */
 
 #if HAVE_LIBINTL
 #  include <libintl.h>		/* For i18n */
@@ -47,6 +48,28 @@ HandleDB(struct PConnection *pconn,
 
 	if (DBINFO_ISRSRC(dbinfo))
 	{
+/* XXX - Crud. There's no function to back up a single database */
+#if 0
+		struct stat statbuf;	/* For stat(), to see if the backup
+					 * file exists.
+					 */
+		const char *bakfname;
+
+		/* See if the backup file exists */
+		bakfname = mkbakfname(dbinfo->name);
+		err = lstat(bakfname, &statbuf);
+		if ((err < 0) && (errno == ENOENT))
+		{
+			SYNC_TRACE(2)
+				fprintf(stderr, "%s doesn't exist. Doing a "
+					"backup\n",
+					bakfname);
+			err = Backup(pconn, palm);
+			MISC_TRACE(2)
+				fprintf(stderr, "Backup() returned %d\n", err);
+		}
+#endif	/* 0 */
+
 		SYNC_TRACE(2)
 			fprintf(stderr,
 				"HandleDB: \"%s\": I don't deal with "
@@ -67,6 +90,84 @@ HandleDB(struct PConnection *pconn,
 	return err;
 }
 
+/* XXX - Should also have, mkatticfname()(?), and function to convert from
+ * escaped names, so can check whether something goes in the attic.
+ */
+
+/* mkfname
+ * Append the name of `dbinfo' to the directory `dirname', escaping any
+ * weird characters so that the result can be used as a pathname.
+ *
+ * If `add_suffix' is true, appends ".pdb" or ".prc", as appropriate, to
+ * the filename.
+ *
+ * Returns a pointer to the resulting filename.
+ */
+const char *
+mkfname(const char *dirname,
+	const struct dlp_dbinfo *dbinfo,
+	Bool add_suffix)
+{
+	static char buf[MAXPATHLEN+1];
+	static char namebuf[(DLPCMD_DBNAME_LEN * 3) + 1];
+				/* Buffer to hold the converted name (with
+				 * all of the weird characters escaped).
+				 * Since an escaped character is 3
+				 * characters long, this buffer need only
+				 * be 3 times the max. length of the
+				 * database name (plus one for the \0).
+				 */
+	int i;
+	char *nptr;
+
+	MISC_TRACE(3)
+		fprintf(stderr, "Inside mkfname(\"%s\",\"%s\")\n",
+			dirname, dbinfo->name);
+	strncpy(buf, dirname, MAXPATHLEN);
+	strncat(buf, "/", MAXPATHLEN-strlen(buf));
+
+	/* If there are any weird characters in the database's name, escape
+	 * them before trying to create a file by that name. Any "weird"
+	 * characters are replaced by "%HH", where HH is the ASCII value
+	 * (hex) of the weird character.
+	 */
+	nptr = namebuf;
+	for (i = 0; i < DLPCMD_DBNAME_LEN; i++)
+	{
+		if (dbinfo->name[i] == '\0')
+			break;
+
+		/* XXX - The isgraph() is mainly for testing. isprint() is
+		 * a better test; spaces are allowed in filenames, and it
+		 * makes the result much more readable.
+		 */
+		if ((!/*isprint*/isgraph(dbinfo->name[i])) ||
+		    (dbinfo->name[i] == '/') ||	/* '/' is a weird character */
+		    (dbinfo->name[i] == '%'))	/* The escape character
+						 * needs to be escaped.
+						 */
+		{
+			/* Escape it */
+			sprintf(nptr, "%%%02x", dbinfo->name[i]);
+			nptr += 3;
+		} else {
+			/* Just a regular character */
+			*nptr = dbinfo->name[i];
+			nptr++;
+		}
+	}
+	*nptr = '\0';
+	strncat(buf, namebuf, MAXPATHLEN-(nptr-namebuf));
+
+	if (add_suffix)
+		strncat(buf, (DBINFO_ISRSRC(dbinfo) ? ".prc" : ".pdb"),
+			MAXPATHLEN-strlen(buf));
+
+	MISC_TRACE(3)
+		fprintf(stderr, "mkfname:    -> \"%s\"\n", buf);
+	return buf;
+}
+
 /* mkbakfname
  * Given a database, construct the standard backup filename for it, and
  * return a pointer to it.
@@ -78,14 +179,13 @@ HandleDB(struct PConnection *pconn,
 const char *
 mkbakfname(const struct dlp_dbinfo *dbinfo)
 {
-	static char buf[MAXPATHLEN+1];
+	return mkfname(backupdir, dbinfo, True);
+}
 
-	strncpy(buf, backupdir, MAXPATHLEN);
-	strncat(buf, "/", MAXPATHLEN-strlen(buf));
-	strncat(buf, dbinfo->name, MAXPATHLEN-strlen(buf));
-	strncat(buf, (DBINFO_ISRSRC(dbinfo) ? ".prc" : ".pdb"),
-		MAXPATHLEN-strlen(buf));
-	return buf;
+const char *
+mkarchfname(const struct dlp_dbinfo *dbinfo)
+{
+	return mkfname(archivedir, dbinfo, False);
 }
 
 /* This is for Emacs's benefit:
