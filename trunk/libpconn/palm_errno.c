a01bf904f887f8d1d775d0ee461408572d34bf64
arensb <arensb@9309df22-992a-11dd-b31e-5b0f3c431db7>
2000-02-06 22:27:07 +0000

    Internationalized error message strings.

--- trunk/libpconn/palm_errno.c
+++ trunk/libpconn/palm_errno.c
@@ -6,9 +6,16 @@
  *	You may distribute this file under the terms of the Artistic
  *	License, as specified in the README file.
  *
- * $Id: palm_errno.c,v 1.2 1999-11-27 05:46:27 arensb Exp $
+ * $Id: palm_errno.c,v 1.3 2000-02-06 22:27:07 arensb Exp $
  */
 #include "config.h"
+
+/* Include I18N-related stuff, if necessary */
+#if HAVE_LIBINTL
+#  include <locale.h>		/* For setlocale() and friends */
+#  include <libintl.h>
+#endif	/* HAVE_LIBINTL */
+
 #include <pconn/palm_errno.h>
 
 int palm_errno;				/* Current error code */
@@ -18,16 +25,16 @@ int palm_errno;				/* Current error code */
  * "palm_errno.h". Make sure these stay in sync.
  */
 const char *palm_errlist[] = {		/* Error messages */
-/* PALMERR_NOERR */	"No error",
-/* PALMERR_SYSTEM */	"Error in system call or library function",
-/* PALMERR_NOMEM */	"Out of memory",
-/* PALMERR_TIMEOUT */	"Timeout",
-/* PALMERR_BADF */	"Bad file descriptor",
-/* PALMERR_EOF */	"End of file",
-/* PALMERR_ABORT */	"Transfer aborted",
-/* PALMERR_BADID */	"Invalid request ID",
-/* PALMERR_BADRESID */	"Invalid result ID",
-/* PALMERR_BADARGID*/	"Invalid argument ID",
+/* PALMERR_NOERR */	N_("No error"),
+/* PALMERR_SYSTEM */	N_("Error in system call or library function"),
+/* PALMERR_NOMEM */	N_("Out of memory"),
+/* PALMERR_TIMEOUT */	N_("Timeout"),
+/* PALMERR_BADF */	N_("Bad file descriptor"),
+/* PALMERR_EOF */	N_("End of file"),
+/* PALMERR_ABORT */	N_("Transfer aborted"),
+/* PALMERR_BADID */	N_("Invalid request ID"),
+/* PALMERR_BADRESID */	N_("Invalid result ID"),
+/* PALMERR_BADARGID*/	N_("Invalid argument ID"),
 };
 /* XXX - Need something to check the size of this array, so if someone adds
  * an error code and forgets to update this array, you can keep from
