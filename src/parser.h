44f753e807686fdf1fcf63436711639fbf2ec607
arensb <arensb@9309df22-992a-11dd-b31e-5b0f3c431db7>
2000-05-06 11:49:06 +0000

    First draft of magic to recognize user-supplied headers.

--- src/parser.h
+++ src/parser.h
@@ -5,7 +5,7 @@
  *	You may distribute this file under the terms of the Artistic
  *	License, as specified in the README file.
  *
- * $Id: parser.h,v 2.4 2000-02-07 04:43:24 arensb Exp $
+ * $Id: parser.h,v 2.5 2000-05-06 11:49:06 arensb Exp $
  */
 #ifndef _parser_h_
 #define _parser_h_
@@ -14,6 +14,7 @@
 #include <sys/param.h>		/* For MAXPATHLEN */
 #include "coldsync.h"
 #include "pconn/pconn.h"	/* For Palm types */
+#include "conduit.h"
 
 /* crea_type_pair
  * A convenience struct that holds a creator-type pair, as long ints.
@@ -25,6 +26,7 @@ typedef struct {
 
 extern int parse_trace;		/* Debugging level for config file parser */
 extern int lineno;		/* Line number */
+extern Bool start_header;	/* See comment in "lexer.l" */
 
 #endif	/* _parser_h_ */
 
