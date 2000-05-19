77bc7053dba808ddf2f01afd7193142cdf5807ab
arensb <arensb@9309df22-992a-11dd-b31e-5b0f3c431db7>
2000-05-19 12:13:10 +0000

    Added declaration for lex_expect(), and symbols for start states.

--- src/parser.h
+++ src/parser.h
@@ -5,7 +5,7 @@
  *	You may distribute this file under the terms of the Artistic
  *	License, as specified in the README file.
  *
- * $Id: parser.h,v 2.5 2000-05-06 11:49:06 arensb Exp $
+ * $Id: parser.h,v 2.6 2000-05-19 12:13:10 arensb Exp $
  */
 #ifndef _parser_h_
 #define _parser_h_
@@ -15,6 +15,12 @@
 #include "coldsync.h"
 #include "pconn/pconn.h"	/* For Palm types */
 #include "conduit.h"
+
+/* Start states.
+ * See lex_expect() in "lexer.l".
+ */
+#define LEX_ERROR	1
+#define LEX_HEADER	2
 
 /* crea_type_pair
  * A convenience struct that holds a creator-type pair, as long ints.
@@ -27,6 +33,8 @@ typedef struct {
 extern int parse_trace;		/* Debugging level for config file parser */
 extern int lineno;		/* Line number */
 extern Bool start_header;	/* See comment in "lexer.l" */
+
+extern void lex_expect(const int state);
 
 #endif	/* _parser_h_ */
 
