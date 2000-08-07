cf0944f01f845be8d12770736304b5dff84a158a
arensb <arensb@9309df22-992a-11dd-b31e-5b0f3c431db7>
2000-08-07 00:46:11 +0000

    Added some standard headers. Added functions pack_dlp_time(), pack_dbinfo(). spc_send() now takes 'pconn' and 'dbinfo' arguments. spc_send() now handles SPCOP_DBINFO and SPCOP_DLPC requests (albeit without any firewall-like error-checking).

--- trunk/src/spc.c
+++ trunk/src/spc.c
@@ -6,14 +6,35 @@
  *	You may distribute this file under the terms of the Artistic
  *	License, as specified in the README file.
  *
- * $Id: spc.c,v 2.1 2000-07-31 09:01:08 arensb Exp $
+ * $Id: spc.c,v 2.2 2000-08-07 00:46:11 arensb Exp $
  */
 
 #include "config.h"
 #include <stdio.h>
+#include <stdlib.h>		/* For malloc() */
+
+#if STDC_HEADERS
+#  include <string.h>		/* For memcpy() */
+#else	/* STDC_HEADERS */
+# ifndef HAVE_STRCHR
+#  define strchr index
+#  define strrchr rindex
+# endif /* HAVE_STRCHR */
+# ifndef HAVE_MEMCPY
+#  define memcpy(d,s,n)		bcopy ((s), (d), (n))
+#  define memmove(d,s,n)	bcopy ((s), (d), (n))
+# endif /* HAVE_MEMCPY */
+#endif	/* STDC_HEADERS */
+
+#include <sys/param.h>		/* For htons() and friends */
 #include "pconn/pconn.h"	/* For DLP and debug_dump() */
 #include "coldsync.h"
 #include "spc.h"
+
+static void pack_dlp_time(const struct dlp_time *t,
+			  unsigned char **ptr);
+static void pack_dbinfo(const struct dlp_dbinfo *dbinfo,
+			unsigned char *buf);
 
 /* spc_send
  * Takes an SPC header, the data associated with it, and a pointer to
@@ -30,10 +51,14 @@
  * afterwards.
  */
 int
-spc_send(struct spc_hdr *header,
-	 const unsigned char *inbuf,
-	 unsigned char **outbuf)
+spc_send(struct spc_hdr *header,		/* SPC header */
+	 struct PConnection *pconn,		/* Connection to Palm */
+	 const struct dlp_dbinfo *dbinfo,	/* Current database */
+	 const unsigned char *inbuf,		/* Request data */
+	 unsigned char **outbuf)		/* Response data (allocated) */
 {
+	int err;
+
 	SYNC_TRACE(5)
 	{
 		fprintf(stderr, "* Inside spc_send(%d, (%d), %ld))\n",
@@ -50,6 +75,62 @@ spc_send(struct spc_hdr *header,
 		*outbuf = NULL;		/* No return data */
 		return 0;
 
+	    case SPCOP_DBINFO:		/* Return information about database */
+		if ((*outbuf = (unsigned char *)
+		     malloc(DLPCMD_DBINFO_LEN + DLPCMD_DBNAME_LEN))
+		    == NULL)
+			return SPCERR_NOMEM;
+		header->len = DLPCMD_DBINFO_LEN + DLPCMD_DBNAME_LEN;
+		pack_dbinfo(dbinfo, *outbuf);
+		header->status = SPCERR_OK;
+		break;
+
+	    case SPCOP_DLPC:		/* Send DLP command to Palm */
+	    {
+		    const ubyte *padp_respbuf;
+		    uword padp_resplen;
+
+		    if (pconn == NULL)	/* Sanity check */
+			    return -1;
+
+		    err = padp_write(pconn, inbuf, header->len);
+		    SYNC_TRACE(7)
+			    fprintf(stderr,
+				    "spc_send: padp_write returned %d\n",
+				    err);
+		    if (err < 0)	/* Problem with padp_write */
+			    /* XXX - Ought to give more information
+			     * than just "it failed".
+			     */
+			    return -1;
+
+		    err = padp_read(pconn, &padp_respbuf, &padp_resplen);
+		    SYNC_TRACE(7)
+		    {
+			    fprintf(stderr,
+				    "spc_send: padp_read returned %d\n",
+				    err);
+			    fprintf(stderr,
+				    "spc_send: padp_resplen == %d\n",
+				    padp_resplen);
+		    }
+		    if (err < 0)	/* Problem with padp_read */
+			    /* XXX - Ought to give more information
+			     * than just "it failed".
+			     */
+			    return -1;
+
+		    *outbuf = malloc(padp_resplen);
+		    if (*outbuf == NULL)	/* Out of memory */
+			    return -1;
+
+		    memcpy(*outbuf, padp_respbuf, padp_resplen);
+		    header->status = SPCERR_OK;
+		    header->len = padp_resplen;
+		    return 0;		/* Success */
+	    }
+		break;
+
 	    default:			/* Bad opcode */
 		header->status = SPCERR_BADOP;
 		header->len = 0L;
@@ -61,4 +142,56 @@ spc_send(struct spc_hdr *header,
 	}
 
 	return 0;		/* Success */
+}
+
+/* pack_dlp_time
+ * Helper function. Writes 't' to the buffer pointed to by 'ptr', and
+ * updates 'ptr' to point to the byte just after that.
+ */
+static void
+pack_dlp_time(const struct dlp_time *t,
+	      unsigned char **ptr)
+{
+	* ((unsigned short *) (*ptr)) = htons(t->year);
+	*ptr += 2;
+	**ptr = t->month;
+	(*ptr)++;
+	**ptr = t->day;
+	(*ptr)++;
+	**ptr = t->hour;
+	(*ptr)++;
+	**ptr = t->minute;
+	(*ptr)++;
+	**ptr = t->second;
+	(*ptr)++;
+	**ptr = 0;
+	(*ptr)++;
+}
+
+/* pack_dbinfo
+ * A private helper function: write 'dbinfo' to the buffer 'buf', with
+ * proper padding and network byte ordering.
+ */
+static void
+pack_dbinfo(const struct dlp_dbinfo *dbinfo,
+	    unsigned char *buf)
+{
+	*buf++ = dbinfo->size;
+	*buf++ = dbinfo->misc_flags;
+	* ((unsigned short *) buf) = htons(dbinfo->db_flags);
+	buf += 2;
+	* ((unsigned long *) buf) = htonl(dbinfo->type);
+	buf += 4;
+	* ((unsigned long *) buf) = htonl(dbinfo->creator);
+	buf += 4;
+	* ((unsigned short *) buf) = htons(dbinfo->version);
+	buf += 2;
+	* ((unsigned long *) buf) = htonl(dbinfo->modnum);
+	buf += 4;
+	pack_dlp_time(&dbinfo->ctime, &buf);
+	pack_dlp_time(&dbinfo->mtime, &buf);
+	pack_dlp_time(&dbinfo->baktime, &buf);
+	* ((unsigned short *) buf) = htons(dbinfo->index);
+	buf += 2;
+	memcpy(buf, dbinfo->name, DLPCMD_DBNAME_LEN);
 }
