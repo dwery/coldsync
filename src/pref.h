04dea96410297e8ec2baa85c40e6a7154070de4c
arensb <arensb@9309df22-992a-11dd-b31e-5b0f3c431db7>
2000-09-01 06:14:54 +0000

    Added copyright statement. Minor cleaning and stylistic changes. Added some "to do" items.

--- src/pref.h
+++ src/pref.h
@@ -2,6 +2,11 @@
  *
  * Definitions, declarations pertaining to the retrieval of preferences.
  * 
+ *	Copyright (C) 2000, Sumant S.R. Oemrawsingh.
+ *	You may distribute this file under the terms of the Artistic
+ *	License, as specified in the README file.
+ *
+ * $Id: pref.h,v 1.1.2.2 2000-09-01 06:14:54 arensb Exp $
  */
 #ifndef _pref_h_
 #define _pref_h_
@@ -9,8 +14,12 @@
 #define PREF_SAVED	0x80	/* In the saved database */
 #define PREF_UNSAVED	0x00	/* In the unsaved database */
 
+#include "config.h"
 #include <coldsync.h>
 
+/* XXX - This shouldn't be everywhere. Move it to pref.c where it's
+ * actually used. Consider making it an inline function.
+ */
 #define prefdesccmp(x,y)	(((x).creator == (y).creator && (x).id == (y).id) ? 0 : 1)
 
 /* struct pref_item
@@ -39,27 +48,23 @@ typedef struct pref_item
 				 */
 } pref_item;
 
-int CacheFromConduits(conduit_block *conduits,struct PConnection *pconn);
-
-int FetchPrefItem(struct PConnection *pconn, pref_item *prefitem);
-
-int DownloadPrefItem(struct PConnection *pconn, pref_item *prefitem);
-
-struct pref_item *
-FindPrefItem(struct pref_desc description,
-	struct pref_item *list);
-
-struct pref_item *
-GetPrefItem(struct pref_desc description);
-
-void
-FreePrefItem(struct pref_item *prefitem);
-
-void
-FreePrefList(struct pref_item *list);
+/* XXX - See whicharguments can be made 'const' */
+extern int CacheFromConduits(conduit_block *conduits,
+			     struct PConnection *pconn);
+extern int FetchPrefItem(struct PConnection *pconn,
+			 pref_item *prefitem);
+extern int DownloadPrefItem(struct PConnection *pconn,
+			    pref_item *prefitem);
+extern struct pref_item *FindPrefItem(struct pref_desc description,
+				      struct pref_item *list);
+extern struct pref_item *GetPrefItem(struct pref_desc description);
+extern void FreePrefItem(struct pref_item *prefitem);
+extern void FreePrefList(struct pref_item *list);
 
 #endif	/* _pref_h_ */
 
-/* Sorry, I use vim, so don't know about the Emacs stuff at the end of your
- * files
+/* This is for Emacs's benefit:
+ * Local Variables:	***
+ * fill-column:	75	***
+ * End:			***
  */
