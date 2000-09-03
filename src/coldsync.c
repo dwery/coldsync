03919642708b96bc8f279f69c6ec8c2af8d2a3ae
arensb <arensb@9309df22-992a-11dd-b31e-5b0f3c431db7>
2000-09-03 04:06:37 +0000

    (bug fix): 'pref_cache' was being freed too soon. Added some trace statements.

--- src/coldsync.c
+++ src/coldsync.c
@@ -4,7 +4,7 @@
  *	You may distribute this file under the terms of the Artistic
  *	License, as specified in the README file.
  *
- * $Id: coldsync.c,v 1.44.2.2 2000-09-01 06:13:25 arensb Exp $
+ * $Id: coldsync.c,v 1.44.2.3 2000-09-03 04:06:37 arensb Exp $
  */
 #include "config.h"
 #include <stdio.h>
@@ -415,6 +415,10 @@ main(int argc, char *argv[])
 	if ((err = ListDBs(pconn, &palm)) < 0)
 	{
 		fprintf(stderr, _("ListDBs returned %d\n"), err);
+
+		MISC_TRACE(6)
+			fprintf(stderr, "Freeing pref_cache\n");
+
 		FreePrefList(pref_cache);
 		Disconnect(pconn, DLPCMD_SYNCEND_CANCEL);
 		pconn = NULL;
@@ -510,6 +514,10 @@ main(int argc, char *argv[])
 		if (err < 0)
 		{
 			fprintf(stderr, _("Error installing new files.\n"));
+
+			MISC_TRACE(6)
+				fprintf(stderr, "Freeing pref_cache\n");
+
 			FreePrefList(pref_cache);
 			Disconnect(pconn, DLPCMD_SYNCEND_CANCEL);
 			pconn = NULL;
@@ -547,6 +555,10 @@ main(int argc, char *argv[])
 				fprintf(stderr, _("Error %d running pre-fetch "
 						  "conduits.\n"),
 					err);
+
+				MISC_TRACE(6)
+					fprintf(stderr,
+						"Freeing pref_cache\n");
 				FreePrefList(pref_cache);
 				Disconnect(pconn, DLPCMD_SYNCEND_CANCEL);
 				pconn = NULL;
@@ -590,6 +602,10 @@ main(int argc, char *argv[])
 
 				Disconnect(pconn, DLPCMD_SYNCEND_OTHER);
 				pconn = NULL;
+
+				MISC_TRACE(6)
+					fprintf(stderr,
+						"Freeing pref_cache\n");
 				FreePrefList(pref_cache);
 				exit(1);
 			}
@@ -615,6 +631,9 @@ main(int argc, char *argv[])
 			fprintf(stderr, _("Error writing user info\n"));
 			Disconnect(pconn, DLPCMD_SYNCEND_OTHER);
 			pconn = NULL;
+
+			MISC_TRACE(6)
+				fprintf(stderr, "Freeing pref_cache\n");
 			FreePrefList(pref_cache);
 			exit(1);
 		}
@@ -630,6 +649,9 @@ main(int argc, char *argv[])
 			fprintf(stderr, _("Error writing sync log.\n"));
 			Disconnect(pconn, DLPCMD_SYNCEND_OTHER);
 			pconn = NULL;
+
+			MISC_TRACE(6)
+				fprintf(stderr, "Freeing pref_cache\n");
 			FreePrefList(pref_cache);
 			exit(1);
 		}
@@ -640,10 +662,15 @@ main(int argc, char *argv[])
 	 * but with a closed connection, they are gonna fail. Although ugly,
 	 * we will manually fill the cache here.
 	 */
-	for(pref_cursor = pref_cache;
-	    pref_cursor != NULL;
-	    pref_cursor = pref_cursor->next)
+	for (pref_cursor = pref_cache;
+	     pref_cursor != NULL;
+	     pref_cursor = pref_cursor->next)
 	{
+		MISC_TRACE(5)
+			fprintf(stderr, "Fetching preference 0x%08lx/%d\n",
+				pref_cursor->description.creator,
+				pref_cursor->description.id);
+
 		if (pref_cursor->contents_info == NULL &&
 		    pconn == pref_cursor->pconn)
 			FetchPrefItem(pconn, pref_cursor);
@@ -652,12 +679,13 @@ main(int argc, char *argv[])
 	/* Finally, close the connection */
 	SYNC_TRACE(3)
 		fprintf(stderr, "Closing connection to Palm\n");
+
 	if ((err = Disconnect(pconn, DLPCMD_SYNCEND_NORMAL)) < 0)
 	{
 		fprintf(stderr, _("Error disconnecting\n"));
 		exit(1);
 	}
-		FreePrefList(pref_cache);
+
 	pconn = NULL;
 
 	/* XXX - The post-dump conduits should only be run if we're
@@ -683,10 +711,11 @@ main(int argc, char *argv[])
 		}
 	}
 
-	MISC_TRACE(1)
-		fprintf(stderr,"Removing preference cache\n");
-
+	MISC_TRACE(6)
+		fprintf(stderr, "Freeing pref_cache\n");
 	FreePrefList(pref_cache);
+
+	MISC_TRACE(1)
 		fprintf(stderr, "ColdSync terminating normally\n");
 	exit(0);
 
