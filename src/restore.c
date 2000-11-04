4ce1e9b7892e33409a341114dd35416bb66fe3a8
arensb <arensb@9309df22-992a-11dd-b31e-5b0f3c431db7>
2000-11-04 23:01:44 +0000

    Restore() eliminated. Its functionality is now split up between restore_file() and restore_dir().

--- src/restore.c
+++ src/restore.c
@@ -7,7 +7,7 @@
  *	You may distribute this file under the terms of the Artistic
  *	License, as specified in the README file.
  *
- * $Id: restore.c,v 2.11 2000-09-08 14:53:00 arensb Exp $
+ * $Id: restore.c,v 2.12 2000-11-04 23:01:44 arensb Exp $
  */
 #include "config.h"
 #include <stdio.h>
@@ -45,15 +45,149 @@
 #include "pdb.h"
 #include "coldsync.h"
 
+/* restore_file
+ * Restore an individual file.
+ */
 int
-Restore(struct PConnection *pconn,
-	struct Palm *palm)		/* XXX - Unused argument */
-	/* XXX - Should take argc, argv arguments. If files are named
-	 * explicitly, restore only those files.
-	 * In fact, if passed the name of a file, restore that file;
-	 * if passed the name of a directory, restore the contents of
-	 * that directory.
+restore_file(struct PConnection *pconn,
+	     struct Palm *palm,
+	     const char *fname)
+{
+	int err;
+	int bakfd;		/* Backup file descriptor */
+	struct pdb *pdb;	/* The backup database */
+
+	/* Read the PDB file */
+	if ((bakfd = open(fname, O_RDONLY | O_BINARY)) < 0)
+	{
+		fprintf(stderr, _("Error: Can't open %s\n"),
+			fname);
+		perror("open");
+		return -1;
+	}
+
+	pdb = pdb_Read(bakfd);
+	if (pdb == NULL)
+	{
+		fprintf(stderr, _("Error reading %s\n"),
+			fname);
+		close(bakfd);
+		return -1;
+	}
+
+	close(bakfd);
+
+	/* XXX - Make sure the database isn't read-only: either it was
+	 * originally a ROM database (and is probably still there), or
+	 * else the database will be impossible to delete.
+	 * XXX - Then again, maybe the intelligent user should be
+	 * allowed to do this.
+	 * XXX - Perhaps best solution is to upload if the user
+	 * specifies -R flag, and discourage people from using it.
 	 */
+	/* XXX - No, the most reasonable thing to do is to check the
+	 * database list in 'palm': if the database already exists,
+	 * delete it. If it's a read-only database, presumably it's a
+	 * ROM database and the delete will fail, in which case skip
+	 * the upload. The question is, is it possible to delete a
+	 * read-only database in RAM?
+	 */
+	SYNC_TRACE(3)
+		fprintf(stderr, "Checking read-only attribute\n");
+	if (pdb->attributes & PDB_ATTR_RO)
+	{
+		fprintf(stderr,
+			_("\"%s\" is a read-only database. "
+			  "Not uploading\n"),
+			fname);
+		return -1;
+	}
+
+	err = DlpDeleteDB(pconn, CARD0, pdb->name);
+	switch (err)
+	{
+	    case DLPSTAT_NOERR:
+	    case DLPSTAT_NOTFOUND:
+		/* If the database wasn't found, that's not an error */
+		break;
+	    case DLPSTAT_DBOPEN:
+		/* Database is already open by someone else. If the
+		 * OKNEWER flag is set, then it's okay to overwrite it
+		 * anyway (e.g., "Graffiti Shortcuts").
+		 */
+	    {
+		    struct dlp_dbinfo *remotedb;
+
+		    /* Look up the database on the Palm. If it has the
+		     * OKNEWER flag set, then go ahead and upload the
+		     * new database anyway.
+		     */
+		    remotedb = find_dbentry(palm, pdb->name);
+		    if (remotedb == NULL)
+		    {
+			    /* This should never happen */
+			    fprintf(stderr,
+				    _("%s: Database %s doesn't exist, "
+				      "yet it is open. Huh?\n"),
+				    "Restore",
+				    pdb->name);
+			    /* But it shouldn't bother us any */
+			    break;
+		    }
+
+		    if ((remotedb->db_flags & DLPCMD_DBFLAG_OKNEWER)
+			== 0)
+		    {
+			    fprintf(stderr,
+				    _("%s: Can't restore %s: it is "
+				      "opened by another application"
+				      "\n"),
+				    "Restore",
+				    pdb->name);
+			    return -1;
+		    }
+
+		    break;	/* Okay to overwrite */
+	    }
+
+	    default:
+		fprintf(stderr,
+			_("Restore: Can't delete database \"%s\"."
+			  " err == %d\n"),
+			pdb->name, err);
+		return -1;
+	}
+
+	/* Call pdb_Upload() to install the file. It shouldn't exist
+	 * anymore by now.
+	 */
+	add_to_log(_("Restore "));
+	add_to_log(pdb->name);
+	add_to_log(" - ");
+	err = pdb_Upload(pconn, pdb);
+	SYNC_TRACE(4)
+		fprintf(stderr, "pdb_Upload returned %d\n", err);  
+	if (err < 0)
+		add_to_log(_("Error\n"));
+	else
+		add_to_log(_("OK\n"));
+	/* XXX - If pdb_Upload returned -1 because of a fatal timeout,
+	 * don't keep trying to upload the others.
+	 */
+
+	/* Free the PDB */
+	free_pdb(pdb);
+
+	return 0;
+}
+
+/* restore_dir
+ * Restore all of the databases in a directory.
+ */
+int
+restore_dir(struct PConnection *pconn,
+	    struct Palm *palm,
+	    const char *dirname)
 {
 	int err;
 	DIR *dir;
@@ -63,14 +197,14 @@ Restore(struct PConnection *pconn,
 
 	MISC_TRACE(1)
 		fprintf(stderr, "Restoring from \"%s\"\n",
-			global_opts.restoredir == NULL ? "(null)" :
-			global_opts.restoredir);
+			dirname == NULL ? "(null)" :
+			dirname);
 
-	dir = opendir(global_opts.restoredir);
+	dir = opendir(dirname);
 	if (dir == NULL)
 	{
 		fprintf(stderr, _("Can't read contents of directory %s\n"),
-			global_opts.restoredir);
+			dirname);
 		perror("opendir");
 		return -1;
 	}
@@ -79,13 +213,10 @@ Restore(struct PConnection *pconn,
 	while ((file = readdir(dir)) != NULL)
 	{
 		char *extptr;		/* Pointer to filename extension */
-		int bakfd;		/* Backup file descriptor */
-		struct pdb *pdb;	/* The backup database */
 
 		SYNC_TRACE(3)
 			fprintf(stderr, "Examining %s/%s\n",
-				global_opts.restoredir,
-				file->d_name);
+				dirname, file->d_name);
 
 /* XXX - This seems to have broken somewhere along the way */
 #if 0
@@ -148,139 +279,21 @@ Restore(struct PConnection *pconn,
 		 * middle, but strnprintf() isn't portable. Then
 		 * again, is it worth caring about?
 		 */
-		strncpy(fname, global_opts.restoredir, MAXPATHLEN);
+		strncpy(fname, dirname, MAXPATHLEN);
 		strncat(fname, "/", MAXPATHLEN-strlen(fname));
 		strncat(fname, file->d_name, MAXPATHLEN-strlen(fname));
 
-		/* Read the PDB file */
-		if ((bakfd = open(fname, O_RDONLY | O_BINARY)) < 0)
-		{
-			fprintf(stderr, _("Error: Can't open %s\n"),
-				fname);
-			perror("open");
-			return -1;
-		}
-
-		pdb = pdb_Read(bakfd);
-		if (pdb == NULL)
-		{
-			fprintf(stderr, _("Error reading %s\n"),
-				fname);
-			close(bakfd);
-			return -1;
-		}
-
-		close(bakfd);
-
-		/* XXX - Make sure the database isn't read-only:
-		 * either it was originally a ROM database (and is
-		 * probably still there), or else the database will be
-		 * impossible to delete.
-		 * XXX - Then again, maybe the intelligent user
-		 * should be allowed to do this.
-		 * XXX - Perhaps best solution is to upload if the
-		 * user specifies -R flag, and discourage people from
-		 * using it.
-		 */
-		/* XXX - No, the most reasonable thing to do is to
-		 * check the database list in 'palm': if the database
-		 * already exists, delete it. If it's a read-only
-		 * database, presumably it's a ROM database and the
-		 * delete will fail, in which case skip the upload.
-		 * The question is, is it possible to delete a
-		 * read-only database in RAM?
-		 */
-		SYNC_TRACE(3)
-			fprintf(stderr, "Checking read-only attribute\n");
-		if (pdb->attributes & PDB_ATTR_RO)
-		{
-			fprintf(stderr,
-				_("\"%s\" is a read-only database. "
-				"Not uploading\n"),
-				file->d_name);
-			continue;
-		}
-
-		err = DlpDeleteDB(pconn, CARD0, pdb->name);
-		switch (err)
-		{
-		    case DLPSTAT_NOERR:
-		    case DLPSTAT_NOTFOUND:
-			/* If the database wasn't found, that's not an
-			 * error
-			 */
-			break;
-		    case DLPSTAT_DBOPEN:
-			/* Database is already open by someone else.
-			 * If the OKNEWER flag is set, then it's okay
-			 * to overwrite it anyway (e.g., "Graffiti
-			 * Shortcuts").
-			 */
-		    {
-			    struct dlp_dbinfo *remotedb;
-
-			    /* Look up the database on the Palm. If it
-			     * has the OKNEWER flag set, then go ahead
-			     * and upload the new database anyway.
-			     */
-			    remotedb = find_dbentry(palm, pdb->name);
-			    if (remotedb == NULL)
-			    {
-				    /* This should never happen */
-				    fprintf(stderr,
-					    _("%s: Database %s doesn't exist, "
-					      "yet it is open. Huh?\n"),
-					    "Restore",
-					    pdb->name);
-				    /* But it shouldn't bother us any */
-				    break;
-			    }
-
-			    if ((remotedb->db_flags & DLPCMD_DBFLAG_OKNEWER)
-				== 0)
-			    {
-				    fprintf(stderr,
-					    _("%s: Can't restore %s: it is "
-					      "opened by another application"
-					      "\n"),
-					    "Restore",
-					    pdb->name);
-				    continue;
-			    }
-
-			    break;	/* Okay to overwrite */
-		    }
-
-		    default:
-			fprintf(stderr,
-				_("Restore: Can't delete database \"%s\"."
-				" err == %d\n"),
-				pdb->name, err);
-			continue;
-		}
-
-		/* Call pdb_Upload() to install the file. It shouldn't
-		 * exist anymore by now.
-		 */
-		add_to_log(_("Restore "));
-		add_to_log(pdb->name);
-		add_to_log(" - ");
-		err = pdb_Upload(pconn, pdb);
-		SYNC_TRACE(4)
-			fprintf(stderr, "pdb_Upload returned %d\n", err);  
-		if (err < 0)
-			add_to_log(_("Error\n"));
-		else
-			add_to_log(_("OK\n"));
-		/* XXX - If pdb_Upload returned -1 because of a fatal
-		 * timeout, don't keep trying to upload the others.
-		 */
-
-		/* Free the PDB */
-		free_pdb(pdb);
+		err = restore_file(pconn, palm, fname);
+		/* XXX - Error-checking */
 	}
 
 	closedir(dir);
 
 	return 0;
 }
+
+/* This is for Emacs's benefit:
+ * Local Variables:	***
+ * fill-column:	75	***
+ * End:			***
+ */
