05f8cfffa33a05833976d01fbd70bea10fd802a5
arensb <arensb@9309df22-992a-11dd-b31e-5b0f3c431db7>
2000-12-13 16:38:44 +0000

    new_PConnection() initializes the common part of 'struct PConnection'.

--- libpconn/PConnection.c
+++ libpconn/PConnection.c
@@ -6,7 +6,7 @@
  *	You may distribute this file under the terms of the Artistic
  *	License, as specified in the README file.
  *
- * $Id: PConnection.c,v 1.11 2000-12-11 08:56:24 arensb Exp $
+ * $Id: PConnection.c,v 1.12 2000-12-13 16:38:44 arensb Exp $
  */
 #include "config.h"
 #include <stdio.h>
@@ -46,6 +46,18 @@ new_PConnection(char *fname, int listenType, int promptHotSync)
 		fprintf(stderr, _("Can't allocate new connection\n"));
 		return NULL;
 	}
+
+	/* Initialize the common part, if only in case the constructor fails */
+	pconn->fd = -1;
+	pconn->io_read = NULL;
+	pconn->io_write = NULL;
+	pconn->io_accept = NULL;
+	pconn->io_drain = NULL;
+	pconn->io_close = NULL;
+	pconn->io_select = NULL;
+	pconn->io_setspeed = NULL;
+	pconn->speed = -1;
+	pconn->io_private = NULL;
 
 	switch (listenType) {
 	    case LISTEN_SERIAL:
