7c6943e98ccf4a61cd2d7d3a5da32c04534e9276
nobody <nobody@9309df22-992a-11dd-b31e-5b0f3c431db7>
2001-01-30 08:14:03 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE-1-6-7'.

---
+++ tags/RELEASE-1-6-7/include/pconn/padp.h
@@ -0,0 +1,124 @@
+/* padp.h
+ *
+ * Types and definitions for Palm Packet Assembly/Disassembly Protocol
+ * (PADP).
+ *
+ *	Copyright (C) 1999-2000, Andrew Arensburger.
+ *	You may distribute this file under the terms of the Artistic
+ *	License, as specified in the README file.
+ *
+ * Structure of a PADP data packet:
+ *	+------+------+
+ *	| type |flags |
+ *	+------+------+
+ *	| size        |
+ *      +------+------+
+ *	| <size> bytes|
+ *	| of data.    |
+ *      +------+------+
+ *
+ * Structure of a PADP ACK packet:
+ *	+------+------+
+ *	| type |flags |
+ *	+------+------+
+ *	| size        |
+ *      +------+------+
+ *
+ * Where:
+ *	'type' is the type of packet. See the PADP_FRAGTYPE_*
+ *	constants, below. Of particular interest here are the 'data'
+ *	and 'ack' types.
+ *	'flags' are flags for the packet. Of particular interest are
+ *	the 'first' flag, which indicates that this packet is the
+ *	first fragment of a message, and the 'last' flag, which
+ *	indicates that this is the last fragment of a message (of
+ *	course, both or neither may be set).
+ *	'size' indicates either the size of the message (in the first
+ *	fragment), or the offset of the fragment in the message (in
+ *	subsequent packets).
+ *
+ * The purpose of PADP is two-fold: first, as its name indicates, it
+ * can break up long packets into smaller fragments, as well as
+ * reassemble them afterwards.
+ * Secondly, it adds reliability (which SLP doesn't provide): each
+ * data packet must be acknowledged with an ACK packet (tickle packets
+ * are not acknowledged). The ACK packet should have the same 'flags'
+ * and 'size' field as the data packet it is in response to[1], as
+ * well as the same SLP transaction ID (see "slp.h"). An ACK packet
+ * should not contain any accompanying data, even if the 'size' field
+ * is nonzero.
+ * If the receiver cannot send an ACK immediately, it should send a
+ * tickle (keepalive) packet every so often, so the connection doesn't
+ * time out.
+ *
+ * [1] exception: the ACK packet may set the "out of memory" flag, to
+ * indicate that the receiving end can't allocate enough memory to
+ * receive the entire packet.
+ *
+ * $Id: padp.h,v 1.6 2001-01-09 16:33:37 arensb Exp $
+ */
+#ifndef _padp_h_
+#define _padp_h_
+
+#include "palm_types.h"
+
+/* PADP fragment types */
+#define PADP_FRAGTYPE_DATA	1	/* User data */
+#define PADP_FRAGTYPE_ACK	2	/* Acknowledgement */
+#define PADP_FRAGTYPE_NAK	3	/* No longer used */
+#define PADP_FRAGTYPE_TICKLE	4	/* Prevent timeouts */
+#define PADP_FRAGTYPE_ABORT	8	/* Abort (1.1 extension) */
+
+/* PADP header flags */
+#define PADP_FLAG_LONGHDR	0x10	/* This is a long header */
+#define PADP_FLAG_ERRNOMEM	0x20	/* Error: receiver is out of memory */
+#define PADP_FLAG_LAST		0x40	/* This is the last fragment */
+#define PADP_FLAG_FIRST		0x80	/* This is the first fragment */
+
+#define PADP_HEADER_LEN		4	/* Length of a PADP header, in bytes */
+#define PADP_MAX_PACKET_LEN	1024	/* Max. length of a single
+					 * PADP packet (not including
+					 * the PADP header). */
+#define PADP_MAX_RETRIES	10	/* # of times to try sending a
+					 * packet. */
+#define PADP_ACK_TIMEOUT	2	/* # seconds to wait for an ACK */
+#define PADP_WAIT_TIMEOUT	30	/* # seconds to wait for the
+					 * next data packet. */
+#define PADP_TICKLE_TIMEOUT	3	/* Time after which to send a
+					 * tickle (in seconds) */
+
+/* PADP error codes */
+#define PADPERR_NOERR		0	/* No error */
+#define PADPERR_BADFD		1	/* Bad file descriptor */
+
+/* padp_header
+ * The header of a PADP packet.
+ *
+ * XXX - the Palm includes indicate that at some point, there will be
+ * a long header type, which supports longer packets than the current
+ * 64Kb maximum. These should be supported.
+ */
+struct padp_header
+{
+	ubyte type;		/* Fragment type (PADP_FRAGTYPE_*) */
+	ubyte flags;		/* Flags */
+	uword size;		/* Size of packet, or offset of fragment */
+};
+
+struct PConnection;		/* Forward declaration */
+
+extern int padp_init(struct PConnection *pconn);
+extern int padp_tini(struct PConnection *pconn);
+
+extern int padp_read(struct PConnection *pconn, const ubyte **buf, uword *len);
+extern int padp_write(struct PConnection *pconn,
+		      const ubyte *buf,
+		      const uword len);
+
+#endif	/* _padp_h_ */
+
+/* This is for Emacs's benefit:
+ * Local Variables: ***
+ * fill-column:	75 ***
+ * End: ***
+ */
---
+++ tags/RELEASE-1-6-7/src/spc.c
@@ -0,0 +1,210 @@
+/* spc.c
+ *
+ * Functions for handling SPC (Serialized Procedure Call) protocol.
+ *
+ *	Copyright (C) 1999-2001, Andrew Arensburger.
+ *	You may distribute this file under the terms of the Artistic
+ *	License, as specified in the README file.
+ *
+ * $Id: spc.c,v 2.6 2001-01-09 16:18:25 arensb Exp $
+ */
+
+#include "config.h"
+#include <stdio.h>
+#include <stdlib.h>		/* For malloc() */
+
+#if STDC_HEADERS
+#  include <string.h>		/* For memcpy() */
+#else	/* STDC_HEADERS */
+# ifndef HAVE_STRCHR
+#  define strchr index
+#  define strrchr rindex
+# endif /* HAVE_STRCHR */
+# ifndef HAVE_MEMCPY
+#  define memcpy(d,s,n)		bcopy ((s), (d), (n))
+#  define memmove(d,s,n)	bcopy ((s), (d), (n))
+# endif /* HAVE_MEMCPY */
+#endif	/* STDC_HEADERS */
+
+#if HAVE_SYS_PARAM_H
+#  include <sys/param.h>		/* For ntohs() and friends */
+#endif	/* HAVE_SYS_PARAM_H */
+#if HAVE_NETINET_IN_H
+#  include <netinet/in.h>		/* For ntohs() and friends, under
+					 * Linux */
+#endif	/* HAVE_NETINET_IN_H */
+
+#include "pconn/pconn.h"	/* For DLP and debug_dump() */
+#include "coldsync.h"
+#include "spc.h"
+
+static void pack_dlp_time(const struct dlp_time *t,
+			  unsigned char **ptr);
+static void pack_dbinfo(const struct dlp_dbinfo *dbinfo,
+			unsigned char *buf);
+
+/* spc_send
+ * Takes an SPC header, the data associated with it, and a pointer to
+ * someplace to return the results.
+ * spc_send() parses the header and does the Right Thing with it,
+ * depending on the opcode in the header. In the case of DLP requests,
+ * this means passing the data along to the Palm and reading a
+ * response.
+ * spc_send() modifies 'header', filling in the return status for the
+ * request, and changing its 'len' field to the length of the return
+ * data.
+ * NB: the data returned in '*outbuf', if any, is allocated by
+ * spc_send(); it is the caller's responsibility to free it
+ * afterwards.
+ */
+int
+spc_send(struct spc_hdr *header,		/* SPC header */
+	 PConnection *pconn,			/* Connection to Palm */
+	 const struct dlp_dbinfo *dbinfo,	/* Current database */
+	 const unsigned char *inbuf,		/* Request data */
+	 unsigned char **outbuf)		/* Response data (allocated) */
+{
+	int err;
+
+	SYNC_TRACE(5)
+	{
+		fprintf(stderr, "* Inside spc_send(%d, (%d), %ld))\n",
+			header->op, header->status, header->len);
+		debug_dump(stderr, "SPC", inbuf, header->len);
+	}
+
+	/* Decide what to do based on the opcode */
+	switch (header->op)
+	{
+	    case SPCOP_NOP:		/* Do nothing */
+		header->status = SPCERR_OK;
+		header->len = 0L;
+		*outbuf = NULL;		/* No return data */
+		return 0;
+
+	    case SPCOP_DBINFO:		/* Return information about database */
+		if ((*outbuf = (unsigned char *)
+		     malloc(DLPCMD_DBINFO_LEN + DLPCMD_DBNAME_LEN))
+		    == NULL)
+			return SPCERR_NOMEM;
+		header->len = DLPCMD_DBINFO_LEN + DLPCMD_DBNAME_LEN;
+		pack_dbinfo(dbinfo, *outbuf);
+		header->status = SPCERR_OK;
+		break;
+
+	    case SPCOP_DLPC:		/* Send DLP command to Palm */
+	    {
+		    /* XXX - Really need to parse the command: a
+		     * conduit can DlpOpenDB() a database, so if it
+		     * dies unexpectedly without a corresponding
+		     * DlpCloseDB(), this can cause a database handle
+		     * leak.
+		     */
+		    const ubyte *padp_respbuf;
+		    uword padp_resplen;
+
+		    if (pconn == NULL)	/* Sanity check */
+			    return -1;
+
+		    err = padp_write(pconn, inbuf, header->len);
+		    SYNC_TRACE(7)
+			    fprintf(stderr,
+				    "spc_send: padp_write returned %d\n",
+				    err);
+		    if (err < 0)	/* Problem with padp_write */
+			    /* XXX - Ought to give more information
+			     * than just "it failed".
+			     */
+			    return -1;
+
+		    err = padp_read(pconn, &padp_respbuf, &padp_resplen);
+		    SYNC_TRACE(7)
+		    {
+			    fprintf(stderr,
+				    "spc_send: padp_read returned %d\n",
+				    err);
+			    fprintf(stderr,
+				    "spc_send: padp_resplen == %d\n",
+				    padp_resplen);
+		    }
+		    if (err < 0)	/* Problem with padp_read */
+			    /* XXX - Ought to give more information
+			     * than just "it failed".
+			     */
+			    return -1;
+
+		    *outbuf = malloc(padp_resplen);
+		    if (*outbuf == NULL)	/* Out of memory */
+			    return -1;
+
+		    memcpy(*outbuf, padp_respbuf, padp_resplen);
+		    header->status = SPCERR_OK;
+		    header->len = padp_resplen;
+		    return 0;		/* Success */
+	    }
+		break;
+
+	    default:			/* Bad opcode */
+		header->status = SPCERR_BADOP;
+		header->len = 0L;
+		*outbuf = NULL;		/* No return data */
+		return 0;	/* Return success, because spc_send()
+				 * successfully processed a malformed
+				 * request.
+				 */
+	}
+
+	return 0;		/* Success */
+}
+
+/* pack_dlp_time
+ * Helper function. Writes 't' to the buffer pointed to by 'ptr', and
+ * updates 'ptr' to point to the byte just after that.
+ */
+static void
+pack_dlp_time(const struct dlp_time *t,
+	      unsigned char **ptr)
+{
+	* ((unsigned short *) (*ptr)) = htons(t->year);
+	*ptr += 2;
+	**ptr = t->month;
+	(*ptr)++;
+	**ptr = t->day;
+	(*ptr)++;
+	**ptr = t->hour;
+	(*ptr)++;
+	**ptr = t->minute;
+	(*ptr)++;
+	**ptr = t->second;
+	(*ptr)++;
+	**ptr = 0;
+	(*ptr)++;
+}
+
+/* pack_dbinfo
+ * A private helper function: write 'dbinfo' to the buffer 'buf', with
+ * proper padding and network byte ordering.
+ */
+static void
+pack_dbinfo(const struct dlp_dbinfo *dbinfo,
+	    unsigned char *buf)
+{
+	*buf++ = dbinfo->size;
+	*buf++ = dbinfo->misc_flags;
+	* ((unsigned short *) buf) = htons(dbinfo->db_flags);
+	buf += 2;
+	* ((unsigned long *) buf) = htonl(dbinfo->type);
+	buf += 4;
+	* ((unsigned long *) buf) = htonl(dbinfo->creator);
+	buf += 4;
+	* ((unsigned short *) buf) = htons(dbinfo->version);
+	buf += 2;
+	* ((unsigned long *) buf) = htonl(dbinfo->modnum);
+	buf += 4;
+	pack_dlp_time(&dbinfo->ctime, &buf);
+	pack_dlp_time(&dbinfo->mtime, &buf);
+	pack_dlp_time(&dbinfo->baktime, &buf);
+	* ((unsigned short *) buf) = htons(dbinfo->index);
+	buf += 2;
+	memcpy(buf, dbinfo->name, DLPCMD_DBNAME_LEN);
+}
