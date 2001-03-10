537d26dd2207d91f3a552edb3f535ef3d113a3ed
arensb <arensb@9309df22-992a-11dd-b31e-5b0f3c431db7>
2001-03-10 05:30:51 +0000

    (portability) Added SIZEOF_* constants.

--- include/pconn/palm_types.h
+++ include/pconn/palm_types.h
@@ -5,7 +5,7 @@
  *	You may distribute this file under the terms of the Artistic
  *	License, as specified in the README file.
  *
- * $Id: palm_types.h,v 1.2 2001-01-09 16:34:31 arensb Exp $
+ * $Id: palm_types.h,v 1.3 2001-03-10 05:30:51 arensb Exp $
  */
 #ifndef _palm_types_h_
 #define _palm_types_h_
@@ -22,6 +22,19 @@ typedef udword chunkID;			/* Those IDs made up of four
 					 * characters stuck together into a
 					 * 32-bit quantity.
 					 */
+
+/* Explicitly define the sizes of types. Can't depend on the host's types
+ * having the same size as the Palm. For instance, Alphas are 64-bit
+ * machines, so 'unsigned long' is 8 bytes, whereas 'udword' is only 4
+ * bytes.
+ */
+#define SIZEOF_BYTE	1
+#define SIZEOF_UBYTE	1
+#define SIZEOF_WORD	2
+#define SIZEOF_UWORD	2
+#define SIZEOF_DWORD	4
+#define SIZEOF_UDWORD	4
+
 /* MAKE_CHUNKID
  * A convenience macro to make a chunkID out of four characters.
  */
