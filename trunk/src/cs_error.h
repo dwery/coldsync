93b4b92a8fd3ccefd30db32b54693c7160fdca30
arensb <arensb@9309df22-992a-11dd-b31e-5b0f3c431db7>
2001-03-18 22:07:56 +0000

    Added an error condition.

--- trunk/src/cs_error.h
+++ trunk/src/cs_error.h
@@ -6,7 +6,7 @@
  *	You may distribute this file under the terms of the Artistic
  *	License, as specified in the README file.
  *
- * $Id: cs_error.h,v 2.1 2000-05-06 11:43:14 arensb Exp $
+ * $Id: cs_error.h,v 2.2 2001-03-18 22:07:56 arensb Exp $
  */
 #ifndef _cs_error_h_
 #define _cs_error_h_
@@ -20,6 +20,7 @@ extern int cs_errno;		/* ColdSync error status */
 					 * Palm" and "killed by Ctrl-C on
 					 * the desktop"?
 					 */
+#define CSE_NOCONN	2		/* Lost connection to Palm */
 
 #endif	/* _cs_error_h_ */
 
