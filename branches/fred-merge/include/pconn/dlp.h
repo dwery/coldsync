8d4323d607b536dd48bffdd504f3ad1dc644bf7a
nobody <nobody@9309df22-992a-11dd-b31e-5b0f3c431db7>
2001-07-30 07:33:27 +0000

    This commit was manufactured by cvs2svn to create branch 'fred-merge'.

---
+++ branches/fred-merge/include/pconn/dlp.h
@@ -0,0 +1,195 @@
+/* dlp.h
+ *
+ * Structures and definitions and such for Palm's Desktop Link
+ * Protocol (DLP).
+ *
+ *	Copyright (C) 1999-2000, Andrew Arensburger.
+ *	You may distribute this file under the terms of the Artistic
+ *	License, as specified in the README file.
+ *
+ * Structure of a DLP request:
+ *	[request header] [arg1] [arg2]...
+ * A DLP request header is of the form
+ *	+------+------+
+ *	| code | argc |
+ *	+------+------+
+ * where 'code' identifies the request, and 'argc' is the number of
+ * arguments following the header.
+ *
+ * Argument headers come in three sizes: tiny, small and long,
+ * depending on the length of the data they contain. Tiny arguments
+ * have <= 256 bytes of payload, small arguments can hold up to 64Kb
+ * of payload, and long arguments can hold up to 4Gb of payload (long
+ * arguments are not yet implemented, but are anticipated for DLP
+ * v2.0). The three types of arguments look like:
+ *
+ * Tiny:
+ *	+------+------+------+------+------+------+
+ *	|  id  | size | <size> bytes of data      |
+ *	+------+------+------+------+------+------+
+ * Small:
+ *	+------+------+------+------+
+ *	|  id  |unused| size        |
+ *	+------+------+------+------+
+ *	| <size> bytes of data      |
+ *	+------+------+------+------+
+ * Long:
+ *	+------+------+------+------+------+------+
+ *	|  id         | size                      |
+ *	+------+------+------+------+------+------+
+ *	| <size> bytes of data                    |
+ *	+------+------+------+------+------+------+
+ * where 'id' identifies the argument (presumably, under DLP, argument
+ * order doesn't matter, since each argument contains a
+ * self-describing ID), and 'size' gives the size of the data.
+ *
+ * The response to a DLP request is of the form:
+ *	[response header] [arg1] [arg2]...
+ * A DLP response header is of the form
+ *	+------+------+------+------+
+ *	| code | argc | errno       |
+ *	+------+------+------+------+
+ * where 'code' identifies the request that this is in response to
+ * (this is the request code, with the high bit set), 'argc' is the
+ * number of arguments following the header, and 'errno' is an error
+ * code to give the status of the response (0 means "no error").
+ * The arguments that follow the header are of the same types as those
+ * in the request. However, even in DLP 2.0, the Palm is not expected
+ * to originate long arguments, even though it will accept them.
+ *
+ * $Id: dlp.h,v 1.5 2001-06-26 05:45:12 arensb Exp $
+ */
+#ifndef _dlp_h_
+#define _dlp_h_
+
+#include "palm.h"
+
+/* These define the version of the DLP protocol that this library
+ * implements.
+ */
+#define DLP_VER_MAJOR		1
+#define DLP_VER_MINOR		2
+
+/* Lengths of various structures */
+#define DLP_REQHEADER_LEN	2	/* Length of request header */
+#define DLP_RESPHEADER_LEN	4	/* Length of response header */
+#define DLP_TINYARG_LEN		2	/* Length of tiny argument */
+#define DLP_TINYARG_MAXLEN	0xff	/* Max. length of tiny argument data */
+#define DLP_SMALLARG_LEN	4	/* Length of small argument */
+#define DLP_SMALLARG_MAXLEN	0xffff	/* Max. length of small argument data */
+#define DLP_LONGARG_LEN		6	/* Length of long argument */
+#define DLP_LONGARG_MAXLEN	0xffffffffL;
+					/* Max. length of long argument data */
+
+/* DLP response status codes */
+#define DLPSTAT_NOERR		0x00	/* No error */
+#define DLPSTAT_SYSTEM		0x01	/* General Palm system error */
+#define DLPSTAT_ILLEGALREQ	0x02	/* Unknown request ID */
+#define DLPSTAT_NOMEM		0x03	/* Insufficient memory */
+#define DLPSTAT_PARAM		0x04	/* Invalid parameter */
+#define DLPSTAT_NOTFOUND	0x05	/* Database, record or
+					 * resource not found */
+#define DLPSTAT_NONEOPEN	0x06	/* There are no open databases */
+#define DLPSTAT_DBOPEN		0x07	/* Database is open by someone else */
+#define DLPSTAT_TOOMANYOPEN	0x08	/* Too many open databases */
+#define DLPSTAT_EXISTS		0x09	/* Database already exists */
+#define DLPSTAT_CANTOPEN	0x0a	/* Can't open database */
+#define DLPSTAT_RECDELETED	0x0b	/* Record is deleted */
+#define DLPSTAT_RECBUSY		0x0c	/* Record is busy */
+#define DLPSTAT_UNSUPP		0x0d	/* Requested operation is not
+					 * supported on the given
+					 * database type */
+#define DLPSTAT_UNUSED1		0x0e
+#define DLPSTAT_READONLY	0x0f	/* You do not have write
+					 * access, or database is in
+					 * ROM */
+#define DLPSTAT_SPACE		0x10	/* Not enough space for
+					 * record/resource/whatever */
+#define DLPSTAT_LIMIT		0x11	/* Size limit exceeded */
+#define DLPSTAT_CANCEL		0x12	/* Cancel the sync */
+#define DLPSTAT_BADWRAP		0x13	/* Bad arg wrapper */
+#define DLPSTAT_NOARG		0x14	/* Required argument not found */
+#define DLPSTAT_ARGSIZE		0x15	/* Invalid argument size */
+
+/* dlp_req_header
+ * DLP request header.
+ */
+struct dlp_req_header
+{
+	ubyte id;		/* Function ID */
+	ubyte argc;		/* # args that follow */
+};
+
+/* dlp_resp_header
+ * DLP response header.
+ */
+struct dlp_resp_header
+{
+	ubyte id;		/* Function ID (what is this a
+				 * response to?) */
+	ubyte argc;		/* # args that follow */
+	uword error;		/* Error code */
+};
+
+/* dlp_tiny_arg
+ * Tiny argument, for values < 256 bytes.
+ */
+struct dlp_tiny_arg
+{
+	ubyte id;		/* Argument ID */
+	ubyte size;		/* Argument size, 0..255 bytes */
+};
+
+/* dlp_small_arg
+ * Small argument, for values between 256 bytes and 64Kb in length.
+ */
+struct dlp_small_arg
+{
+	ubyte id;		/* Argument ID */
+	ubyte unused;		/* Dummy for alignment. Set to 0 */
+	uword size;		/* Argument size, 0..65535 bytes */
+};
+
+/* dlp_long_arg
+ * Long argument, for values longer than 64Kb.
+ *
+ * 2.0 extension: not yet implemented. The Palm will not originate
+ * these, but it will accept them.
+ */
+struct dlp_long_arg
+{
+	uword id;		/* Argument ID */
+	udword size;		/* Argument size, 0..0xffffffff bytes */
+};
+
+/* dlp_arg
+ * This is a generic argument holder. This is not part of PalmOS or
+ * HotSync, but rather a convenience type, for passing to
+ * dlp_send_req().
+ */
+struct dlp_arg
+{
+	uword id;		/* Argument ID */
+	udword size;		/* Argument size, 0..0xffffffff bytes */
+	ubyte *data;		/* The argument data itself */
+};
+
+struct PConnection;		/* Forward declaration */
+
+extern int dlp_init(struct PConnection *pconn);
+extern int dlp_tini(struct PConnection *pconn);
+
+/* Protocol functions */
+extern int dlp_send_req(struct PConnection *pconn,
+			struct dlp_req_header *header,
+			struct dlp_arg argv[]);
+extern int dlp_recv_resp(struct PConnection *pconn, const ubyte id,
+			 struct dlp_resp_header *header,
+			 const struct dlp_arg **argv);
+#endif	/* _dlp_h_ */
+
+/* This is for Emacs's benefit:
+ * Local Variables: ***
+ * fill-column:	75 ***
+ * End: ***
+ */
---
+++ branches/fred-merge/include/pconn/netsync.h
@@ -0,0 +1,124 @@
+/* netsync.c
+ *
+ * Declarations and definitions for stuff related to network syncing.
+ *
+ *	Copyright (C) 2000, Andrew Arensburger.
+ *	You may distribute this file under the terms of the Artistic
+ *	License, as specified in the README file.
+ *
+ * NetSync outline:
+ *
+ * This packet encapsulation protocol is used for both 'NetSync' and
+ * talking to the Palm m50x in its USB Palm cradle.
+ *
+ * Data goes back and forth in the following format:
+ *
+ *	+------+------+
+ *	| cmd  | xid  |
+ *	+------+------+------+------+
+ *	| length                    |
+ *	+------+------+------+------+
+ *	| <length> bytes of data...
+ *	+------+------+------+------+
+ *
+ * Where <cmd> always appears to be 1; <xid> is a transaction ID or
+ * sequence number: it starts at 0xff, and is incremented with each command
+ * that the server sends.
+ * 'pilot-link' treats the xid the same way as for SLP: once it hits 0xfe,
+ * it is incremented directly to 0x01. It remains to be seen whether this
+ * is necessary.
+ *
+ * The data portion of the packet typically appears to be a DLP packet
+ * (except for the beginning of the session).
+ *
+ * (Note: most all of this is conjecture: it could be that the <length>
+ * field is really a short flags field followed by a length field.)
+ *
+ * $Id: netsync.h,v 1.5 2001-07-30 07:06:15 arensb Exp $
+ */
+
+#ifndef _netsync_h_
+#define _netsync_h_
+
+#include <sys/param.h>		/* For MAXHOSTNAMELEN */
+#include "pconn.h"
+#include "palm.h"
+
+/*
+ * Ritual statements
+ * These packets are sent back and forth during the initial handshaking
+ * phase. I don't know what they mean. The sequence is:
+ * client sends UDP wakeup packet
+ * server sends UDP wakeup ACK
+ * client sends ritual response 1
+ * server sends ritual statement 2
+ * client sends ritual response 2
+ * server sends ritual statement 3
+ * client sends ritual response 3
+ *
+ * The comments are mostly conjecture and speculation.
+ */
+extern ubyte ritual_resp1[];
+#define ritual_resp1_size 22
+extern ubyte ritual_stmt2[];
+#define ritual_stmt2_size 50
+extern ubyte ritual_resp2[];
+#define ritual_resp2_size 50
+extern ubyte ritual_stmt3[];
+#define ritual_stmt3_size 46
+extern ubyte ritual_resp3[];
+#define ritual_resp3_size 8
+
+/* struct netsync_wakeup
+ * At the beginning of the NetSync process, the client sends one or more
+ * UDP packets with this structure to the server.
+ */
+struct netsync_wakeup
+{
+	uword magic;		/* Magic value identifying a wakeup packet */
+	ubyte type;		/* Packet type? */
+	ubyte unknown;		/* Might be the length of something */
+				/* XXX - Find out what this is */
+	udword hostid;		/* Server's host ID. Normally, this should
+				 * be its IP address, but presumably this
+				 * might be any unique identifier.
+				 */
+	udword netmask;		/* Netmask of <hostid>. Typically
+				 * 0xffffff00.
+				 */
+	char hostname[DLPCMD_MAXHOSTNAMELEN];
+				/* Server host name */
+};
+
+struct netsync_header
+{
+	ubyte cmd;		/* Command type. Always 1, AFAICT. */
+	ubyte xid;		/* Sequence number (transaction ID) */
+	udword len;		/* Length of payload */
+};
+
+#define NETSYNC_HDR_LEN	6	/* Length of NetSync header */
+
+/* Protocol functions */
+extern int netsync_init(PConnection *pconn);
+extern int netsync_tini(PConnection *pconn);
+extern int netsync_read(PConnection *pconn,
+			const ubyte **buf,
+			uword *len);
+extern int netsync_read_method(PConnection *pconn,
+			       const ubyte **buf,
+			       uword *len,
+			       const Bool no_header);
+extern int netsync_write(PConnection *pconn,
+			 const ubyte *buf,
+			 const uword len);
+extern int ritual_exch_client(PConnection *pconn);
+extern int ritual_exch_server(PConnection *pconn);
+
+#endif	/* _netsync_h_ */
+
+/* This is for Emacs's benefit:
+ * Local Variables:	***
+ * fill-column:	75	***
+ * End:			***
+ */
