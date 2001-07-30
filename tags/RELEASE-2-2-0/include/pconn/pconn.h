24b804213e32c3b5d6ec74dda47181d1e191c967
nobody <nobody@9309df22-992a-11dd-b31e-5b0f3c431db7>
2001-07-30 07:33:26 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE-2-2-0'.

---
+++ tags/RELEASE-2-2-0/include/pconn/pconn.h
@@ -0,0 +1,60 @@
+/* pconn.h
+ * Simple include file that includes all of the pconn-related .h files
+ *
+ *	Copyright (C) 1999-2001, Andrew Arensburger.
+ *	You may distribute this file under the terms of the Artistic
+ *	License, as specified in the README file.
+ *
+ * $Id: pconn.h,v 1.8 2001-07-26 06:58:58 arensb Exp $
+ */
+#ifndef __pconn_h__
+#define __pconn_h__
+
+#define CARD0	0		/* Memory card #0. The only real purpose of
+				 * this is to make it marginally easier to
+				 * find all of the places where card #0 has
+				 * been hardcoded, once support for
+				 * multiple memory cards is added, if it
+				 * ever is.
+				 */
+
+#define NETSYNC_WAKEUP_MAGIC    0xfade
+#define NETSYNC_WAKEUP_PORT     14237   /* UDP port on which the client
+                                         * sends out the wakeup request.
+                                         */
+#define NETSYNC_DATA_PORT       14238   /* TCP port on which the client and
+                                         * server exchange sync data.
+                                         */
+
+/* Debugging variables */
+extern int slp_trace;		/* Debugging level for Serial Link Protocol */
+extern int cmp_trace;		/* Debugging level for Connection
+				 * Management Protocol */
+extern int padp_trace;		/* Debugging level for Packet
+				 * Assembly/Disassembly Protocol. */
+extern int dlp_trace;		/* Debugging level for Desktop Link
+				 * Protocol */
+extern int dlpc_trace;		/* Debugging level for Desktop Link
+				 * Protocol commands */
+
+#include <pconn/dlp_cmd.h>
+
+/* XXX - Instead of including a bunch of files here, should just put
+ * all of the libpconn prototypes here.
+ */
+#include <pconn/palm_errno.h>
+#include <pconn/cmp.h>
+#include <pconn/dlp_cmd.h>
+#include <pconn/dlp_rpc.h>
+#include <pconn/netsync.h>
+#include <pconn/util.h>
+
+extern uword crc16(const ubyte *buf, uword len, const uword start);
+
+#endif	/* __pconn_h__ */
+
+/* This is for Emacs's benefit:
+ * Local Variables: ***
+ * fill-column:	75 ***
+ * End: ***
+ */
---
+++ tags/RELEASE-2-2-0/src/palment.h
@@ -0,0 +1,58 @@
+/* palment.h
+ *
+ *	Copyright (C) 2001, Andrew Arensburger.
+ *	You may distribute this file under the terms of the Artistic
+ *	License, as specified in the README file.
+ *
+ * Definitions and declarations pertaining to /etc/palms, the database
+ * of Palms.
+ *
+ * The structure of /etc/palms is reminiscent of /etc/passwd: each
+ * line represents one Palm; fields in each line are separated by '|'.
+ * The fields are, in order,
+ *	serial|username|userid|luser|name|conf_fname
+ *
+ * serial:	The Palm's serial number
+ * username:	The username on the Palm
+ * userid:	The user ID on the Palm
+ * luser:	Local (Unix) user to setuid() to
+ * name:	Palm's name in .coldsyncrc
+ * conf_fname:	Config file to use
+ *
+ * 'name' and 'conf_fname' are optional.
+ *
+ * $Id: palment.h,v 2.3 2001-03-28 04:55:36 arensb Exp $
+ */
+#ifndef _palment_h_
+#define _palment_h_
+
+#include "config.h"
+
+/* Path to /etc/palms */
+#define _PATH_PALMS	SYSCONFDIR "/palms"
+
+struct palment
+{
+	const char *serial;		/* Palm serial number */
+	const char *username;		/* Username on Palm */
+	unsigned long userid;		/* User ID on Palm */
+	const char *luser;		/* Local user (Unix user) */
+	const char *name;		/* Palm name */
+	const char *conf_fname;		/* Path to config file to use */
+	/* XXX - Should this also include an entry for a Palm to forward
+	 * the connection to?
+	 */
+};
+
+extern const struct palment *getpalment(void);
+/* XXX - extern const struct palment *getpalmbyname(const char *name); */
+extern void setpalment(int stayopen);
+extern void endpalment(void);
+
+#endif	/* _palment_h_ */
+
+/* This is for Emacs's benefit:
+ * Local Variables:	***
+ * fill-column:	75	***
+ * End:			***
+ */
