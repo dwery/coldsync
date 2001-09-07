8dd5136fec36e3879d530f58709af9ad1a5c3b21
arensb <arensb@9309df22-992a-11dd-b31e-5b0f3c431db7>
2001-09-07 04:31:09 +0000

    Defined enums for SPCOP_* and SPCERR_* constants. Not as useful as might be hoped, but every bit counts.

--- src/spc.h
+++ src/spc.h
@@ -7,17 +7,40 @@
  *	You may distribute this file under the terms of the Artistic
  *	License, as specified in the README file.
  *
- * $Id: spc.h,v 2.4 2000-12-24 21:25:01 arensb Exp $
+ * $Id: spc.h,v 2.5 2001-09-07 04:31:09 arensb Exp $
  *
  * Structure of an SPC request:
  *	[request header] [data]
  * The header contains an opcode and the length of the data. The form of
- * the depends entirely on the opcode.
+ * the data depends on the opcode.
+ * XXX - Describe this in more detail.
  */
 #ifndef _spc_h_
 #define _spc_h_
 
 #include "config.h"
+
+/* SPC opcodes. The response opcodes have the same values as the request
+ * opcodes, but with the high bit set.
+ */
+typedef enum {
+	SPCOP_NOP = 0,		/* Do nothing. */
+	SPCOP_DBINFO,		/* Request information about current
+				 * database */
+	SPCOP_DLPC,		/* DLP command */
+	SPCOP_DLPR		/* RPC over DLP */
+				/* XXX - Is this a good idea? Would it be
+				 * better to force applications to send
+				 * preformatted RPC-over-DLP packets?
+				 */
+} SPC_Op;
+
+/* Status/error codes */
+typedef enum {
+	SPCERR_OK = 0,		/* No error */
+	SPCERR_BADOP,		/* Unknown opcode */
+	SPCERR_NOMEM		/* Out of memory */
+} SPC_Errno;
 
 /* spc_req_hdr
  * Header of an SPC request.
@@ -25,32 +48,14 @@
 struct spc_hdr {
 	unsigned short op;	/* Opcode: what kind of request is this?
 				 * This is one of the SPCOP_* constants,
-				 * below. */
+				 * above. */
 	unsigned short status;	/* Return status. Ignored when sending
 				 * request. In the response, this is one of
-				 * the SPCERR_* constants, below */
+				 * the SPCERR_* constants, above */
 	unsigned long len;	/* Length of data following this header */
 };
 
 #define SPC_HEADER_LEN	8	/* Length of SPC header */
-
-/* SPC opcodes. The response opcodes have the same values as the request
- * opcodes, but with the high bit set.
- */
-#define SPCOP_NOP	0	/* Do nothing. */
-#define SPCOP_DBINFO	1	/* Request information about current
-				 * database */
-#define SPCOP_DLPC	2	/* DLP command */
-#define SPCOP_DLPR	3	/* RPC over DLP */
-				/* XXX - Is this a good idea? Would it be
-				 * better to force applications to send
-				 * preformatted RPC-over-DLP packets?
-				 */
-
-/* Status/error codes */
-#define SPCERR_OK	0	/* No error */
-#define SPCERR_BADOP	1	/* Unknown opcode */
-#define SPCERR_NOMEM	2	/* Out of memory */
 
 extern int spc_send(struct spc_hdr *header,
 		    PConnection *pconn,
