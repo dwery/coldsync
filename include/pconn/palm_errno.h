b5a1d27928265c4bae6d00214676c26539335db7
arensb <arensb@9309df22-992a-11dd-b31e-5b0f3c431db7>
2001-09-08 01:10:01 +0000

    Added type palmerr_t for libpconn status/error codes. Changed palm_errno to palmerr_t. Added declaration for palm_strerror().

--- include/pconn/palm_errno.h
+++ include/pconn/palm_errno.h
@@ -6,30 +6,32 @@
  *	You may distribute this file under the terms of the Artistic
  *	License, as specified in the README file.
  *
- * $Id: palm_errno.h,v 1.3 2001-04-15 04:33:06 arensb Exp $
+ * $Id: palm_errno.h,v 1.4 2001-09-08 01:10:01 arensb Exp $
  */
 #ifndef _palm_errno_h_
 #define _palm_errno_h_
 
 /* Error codes */
-#define PALMERR_NOERR		0	/* No error */
-#define PALMERR_SYSTEM		1	/* Error in system call.
+typedef enum {
+	PALMERR_NOERR		= 0,	/* No error */
+	PALMERR_SYSTEM,			/* Error in system call.
 					 * Consult `errno' */
-#define PALMERR_NOMEM		2	/* Out of memory */
-#define PALMERR_TIMEOUT		3	/* A timeout occurred */
-#define PALMERR_BADF		4	/* Bad file descriptor */
-#define PALMERR_EOF		5	/* End of file */
-#define PALMERR_ABORT		6	/* Palm has aborted */
-#define PALMERR_BADID		7	/* Invalid request ID */
-#define PALMERR_BADRESID	8	/* Invalid result ID */
-#define PALMERR_BADARGID	9	/* Invalid argument ID */
-#define PALMERR_ACKXID		10	/* XID on ACK doesn't match request */
+	PALMERR_NOMEM,			/* Out of memory */
+	PALMERR_TIMEOUT,		/* A timeout occurred */
+	PALMERR_BADF,			/* Bad file descriptor */
+	PALMERR_EOF,			/* End of file */
+	PALMERR_ABORT,			/* Palm has aborted */
+	PALMERR_BADID,			/* Invalid request ID */
+	PALMERR_BADRESID,		/* Invalid result ID */
+	PALMERR_BADARGID,		/* Invalid argument ID */
+	PALMERR_ACKXID			/* XID on ACK doesn't match request */
+} palmerr_t;
 
-extern int palm_errno;			/* Error code */
+extern palmerr_t palm_errno;		/* Error code */
 extern const char *palm_errlist[];	/* List of error messages */
 extern const int palm_numerrs;		/* Length of palm_errlist */
 
-/* XXX - palm_perror(), perhaps? */
+extern const char *palm_strerror(const palmerr_t palm_errno);
 
 #endif	/* _palm_errno_h_ */
 
