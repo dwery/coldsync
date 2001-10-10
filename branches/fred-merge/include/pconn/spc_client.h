787a3799f17465bccb7be9a5be093c7faa9ff01b
arensb <arensb@9309df22-992a-11dd-b31e-5b0f3c431db7>
2001-10-10 23:59:21 +0000

    (added): Declarations for SPC client.

---
+++ branches/fred-merge/include/pconn/spc_client.h
@@ -0,0 +1,25 @@
+/* spc_client.h
+ *
+ * Allows a conduit to connect to the Palm device using SPC.
+ *
+ *	Copyright (C) 2001, Andrew Arensburger.
+ *	You may distribute this file under the terms of the Artistic
+ *	License, as specified in the README file.
+ *
+ * This file was created by Fred Gylys-Colwell
+ *
+ * $Id: spc_client.h,v 1.1.4.1 2001-10-10 23:59:21 arensb Exp $
+ */
+#ifndef _spc_client_h_
+#define _spc_client_h_
+
+#include "pconn/PConnection.h"
+#include "pconn/dlp_cmd.h"
+#include "pconn/netsync.h"
+
+/* Create a PConnection for a conduit to using SLP. */
+extern PConnection *new_spc_client(int fd);
+/* Get the database information  using SPC.*/
+extern int spc_get_dbinfo(PConnection *pconn, struct dlp_dbinfo *info);
+
+#endif	/* _spc_client_h_ */
