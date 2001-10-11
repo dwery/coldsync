f0d63ade034a25baa1d77d53a58f8f7ec51f420a
arensb <arensb@9309df22-992a-11dd-b31e-5b0f3c431db7>
2001-10-11 02:51:01 +0000

    Fixed up comments to fit my style. Enclosed the whole thing in 'extern "C"'

--- src/symboltable.h
+++ src/symboltable.h
@@ -2,24 +2,25 @@
  *
  * A symbol table for use in the parser.
  *
- *	Copyright (C) 1999-2001, Andrew Arensburger.
+ *	Copyright (C) 2001, Andrew Arensburger.
  *	You may distribute this file under the terms of the Artistic
  *	License, as specified in the README file.
  *
- * $Id: symboltable.h,v 1.1.2.1 2001-10-09 01:41:28 arensb Exp $
+ * $Id: symboltable.h,v 1.1.2.2 2001-10-11 02:51:01 arensb Exp $
  */
 
-
-/* Get a symbol from the table that matches the give key.  
- */
+extern "C" {
 extern char *get_symbol(const char *name);
-/* Get a symbol from the table that matches the give key.  The key has
- * length len, and does not have to be null terminated.
- */
+			/* Get a symbol from the table that matches
+			 * the given key. */
 extern char *get_symbol_n(const char *name, int len);
-/* Puy a symbol onto the table. (name is the key, value is the value. )
- */
+			/* Get a symbol from the table that matches
+			 * the given key. The key has length len, and
+			 * does not have to be NUL-terminated. */
 extern void put_symbol(const char *name, const char *value);
-
-/* Initialize the symbol table based on the arguments. */
+			/* Put a symbol onto the table. (name is the
+			 * key, value is the value.) */
 extern void symboltable_init();
+			/* Initialize the symbol table based on the
+			 * arguments. */
+};
