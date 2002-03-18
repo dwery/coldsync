aaeb0096d879a37611104a808e27af10d85bb9dc
arensb <arensb@9309df22-992a-11dd-b31e-5b0f3c431db7>
2002-03-18 08:28:41 +0000

    Got rid of get_symbol_n(). put_symbol() returns error code. Added symboltable_tini().

--- src/symboltable.h
+++ src/symboltable.h
@@ -2,12 +2,13 @@
  *
  * A symbol table for use in the parser.
  *
- *	Copyright (C) 2001, Andrew Arensburger.
+ *	Copyright (C) 2001-2002, Andrew Arensburger.
  *	You may distribute this file under the terms of the Artistic
  *	License, as specified in the README file.
  *
- * $Id: symboltable.h,v 2.3 2001-10-18 01:39:10 arensb Exp $
+ * $Id: symboltable.h,v 2.4 2002-03-18 08:28:41 arensb Exp $
  */
+/* XXX - Redo the API: get_symbol() ought to return a (struct symbol *). */
 
 #ifdef __cplusplus
 extern "C" {
@@ -15,16 +16,21 @@ extern "C" {
 extern char *get_symbol(const char *name);
 			/* Get a symbol from the table that matches
 			 * the given key. */
-extern char *get_symbol_n(const char *name, int len);
-			/* Get a symbol from the table that matches
-			 * the given key. The key has length len, and
-			 * does not have to be NUL-terminated. */
-extern void put_symbol(const char *name, const char *value);
+			/* XXX - This looks in the environment by default.
+			 * But "config.c" uses symbol "LOGFILE", which
+			 * seems a bit too generic. So perhaps there ought
+			 * to be a separate get_symbol_env() which looks in
+			 * the environment. Perhaps it should take a
+			 * separate argument giving the name of the
+			 * environment variable to look up.
+			 */
+extern int put_symbol(const char *name, const char *value);
 			/* Put a symbol onto the table. (name is the
 			 * key, value is the value.) */
 extern void symboltable_init(void);
-			/* Initialize the symbol table based on the
-			 * arguments. */
+			/* Initialize the symbol table */
+extern void symboltable_tini(void);
+			/* Clean up the symbol table */
 #ifdef __cplusplus
 };
 #endif	/* __cplusplus */
