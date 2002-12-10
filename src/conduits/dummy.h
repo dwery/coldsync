d16333a6a3359653ecc033e76d78b0d20d2bd0c2
azummo <azummo@9309df22-992a-11dd-b31e-5b0f3c431db7>
2002-12-10 11:54:51 +0000

    Moved the dummy conduit to a separate file.

---
+++ src/conduits/dummy.h
@@ -0,0 +1,32 @@
+/* dummy.h
+ *
+ * Dummy conduit, header file.
+ *
+ *	Copyright (C) 1999-2001, Andrew Arensburger.
+ *	You may distribute this file under the terms of the Artistic
+ *	License, as specified in the README file.
+ *
+ * $Id: dummy.h,v 1.1 2002-12-10 11:54:51 azummo Exp $
+ */
+
+#include "config.h"
+#include <stdio.h>
+#include <stdlib.h>			/* For malloc(), free() */
+
+#if HAVE_LIBINTL_H
+#  include <libintl.h>			/* For i18n */
+#endif	/* HAVE_LIBINTL_H */
+
+#include <pconn/pconn.h>
+#include "conduit.h"
+
+extern int run_DummyConduit(PConnection *pconn,
+		     const struct dlp_dbinfo *dbinfo,
+		     const conduit_block *block,
+		     const pda_block *pda);
+
+/* This is for Emacs's benefit:
+ * Local Variables: ***
+ * fill-column:	75 ***
+ * End: ***
+ */
